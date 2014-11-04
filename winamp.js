0abdf0401f420bfff8929e4b5f53a4e2e2723d54
Jordan Eldredge <jordaneldredge@gmail.com>
2014-11-03 22:12:46 -0500

    Change location of llama.mp3

--- winamp.js
+++ winamp.js
@@ -306,4 +306,4 @@ document.onkeyup = function(e){
 }
 
 winamp = new Winamp();
-winamp.loadFile('http://jordaneldredge.com/projects/winamp.js/llama.mp3', 'llama.mp3');
+winamp.loadFile('http://jordaneldredge.com/projects/winamp2-js/llama.mp3', 'llama.mp3');
