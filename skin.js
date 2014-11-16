56db746c353a84f5d4a5ced2732a916dc648da5b
Elliot Shepherd <elliot@ninjablocks.com>
2014-11-16 17:20:25 +1100

    Load skins from original WSZ files instead of individual BMPs

--- skin.js
+++ skin.js
@@ -35,32 +35,112 @@ SkinManager = function() {
     }
 
     // For local dev, we want to use the asset we have locally
-    this.useLocalDefaultSkin = function() {
-        self.setSkinByUrl('skins/default');
+    this.setSkinByName = function() {
+        self.setSkinByWszUrl('default');
     }
 
     // I have a collection of skins on GitHub to make loading remote skins
     // easier. rawgit.com changes this into a free CDN
     this.setSkinByName = function(name) {
-        url = "https://cdn.rawgit.com/captbaritone/winamp-skins/master/v2/" + name;
+        url = "https://cdn.rawgit.com/elliots/winamp2-js/master/skins/" + name + ".wsz";
         self.setSkinByUrl(url);
     }
 
-    // Given the URL of a skin directory, set the current skin
-    this.setSkinByUrl = function(skinPath) {
-        // Make sure we have a trailing slash. Two slashes are > than none
-        skinPath += "/";
+    // Given the url of an original Winamp WSZ file, set the current skin
+    this.setSkinByUrl = function(url) {
 
-        var style = document.getElementById('skin');
-        // XXX Ideally we would empty the style tag here, but I don't know how.
-        // Appending overwrites, which has the same net effect, but after
-        // several skin changes, this tag will get pretty bloated.
-        var cssRules = '';
-        for(var selector in self._skinImages) {
-            var imagePath = skinPath + self._skinImages[selector];
-            var value = "background-image: url(" + imagePath + ");";
-            cssRules += selector + "{" + value + "}\n";
-            style.appendChild(document.createTextNode(cssRules));
+      JSZipUtils.getBinaryContent(url, function(err, data) {
+
+        if (err) {
+          alert('Failed to load skin ' + url + ' : ' + err);
         }
+
+        try {
+
+          var zip = new JSZip(data);
+
+          var style = document.getElementById('skin');
+          var cssRules = '';
+          for(var selector in self._skinImages) {
+
+            var file = zip.filter(function (relativePath, file){
+              return new RegExp(self._skinImages[selector], 'i').test(relativePath)
+            })[0];
+
+            if (!file) {
+              console.log("Warning: Couldn't find file:" + self._skinImages[selector])
+            } else {
+              var value = "background-image: url(data:image/bmp;base64," + btoa(file.asBinary()) + ")"
+              cssRules += selector + "{" + value + "}\n";
+            }
+          }
+          style.appendChild(document.createTextNode(cssRules));
+
+        } catch(e) {
+          alert('Failed to load skin ' + url + ' : ' + e.message);
+        }
+      })
+
     }
 }
+
+;(function () {
+
+  var object = typeof exports != 'undefined' ? exports : this; // #8: web workers
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+  function InvalidCharacterError(message) {
+    this.message = message;
+  }
+  InvalidCharacterError.prototype = new Error;
+  InvalidCharacterError.prototype.name = 'InvalidCharacterError';
+
+  // encoder
+  // [https://gist.github.com/999166] by [https://github.com/nignag]
+  object.btoa || (
+  object.btoa = function (input) {
+    for (
+      // initialize result and counter
+      var block, charCode, idx = 0, map = chars, output = '';
+      // if the next input index does not exist:
+      //   change the mapping table to "="
+      //   check if d has no fractional digits
+      input.charAt(idx | 0) || (map = '=', idx % 1);
+      // "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
+      output += map.charAt(63 & block >> 8 - idx % 1 * 8)
+    ) {
+      charCode = input.charCodeAt(idx += 3/4);
+      if (charCode > 0xFF) {
+        throw new InvalidCharacterError("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+      }
+      block = block << 8 | charCode;
+    }
+    return output;
+  });
+
+  // decoder
+  // [https://gist.github.com/1020396] by [https://github.com/atk]
+  object.atob || (
+  object.atob = function (input) {
+    input = input.replace(/=+$/, '');
+    if (input.length % 4 == 1) {
+      throw new InvalidCharacterError("'atob' failed: The string to be decoded is not correctly encoded.");
+    }
+    for (
+      // initialize result and counters
+      var bc = 0, bs, buffer, idx = 0, output = '';
+      // get next character
+      buffer = input.charAt(idx++);
+      // character found in table? initialize bit storage and add its ascii value;
+      ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+        // and if not first of each 4 characters,
+        // convert the first 8 bits to one ascii character
+        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+    ) {
+      // try to find character in table (0-63, not found => -1)
+      buffer = chars.indexOf(buffer);
+    }
+    return output;
+  });
+
+}());
