aa657a713da946f8eaa6018dde07b773c76461e7
PAEz <cdkpaez@gmail.com>
2014-12-04 01:50:34 +1030

    FileManager

--- file-manager.js
+++ file-manager.js
@@ -1,29 +1,38 @@
-// Handle common file interactions
-FileManager = {
-    reader: new FileReader(),
+(function(window, undefined) {
 
-    bufferFromFileReference: function(fileReference, bufferHandler) {
-        this.reader.onload = function (e) {
-            var arrayBuffer = e.target.result;
-            bufferHandler(arrayBuffer);
-        };
-        this.reader.onerror = function (e) {
-            //console.error(e);
-        };
+	var reader = new FileReader();
 
-        this.reader.readAsArrayBuffer(fileReference);
-    },
+	function bufferFromFileReference(fileReference, bufferHandler) {
+		reader.onload = function(e) {
+			var arrayBuffer = e.target.result;
+			bufferHandler(arrayBuffer);
+		};
+		reader.onerror = function(e) {
+			//console.error(e);
+		};
 
-    bufferFromUrl: function(url, bufferHandler) {
-        var oReq = new XMLHttpRequest();
-        oReq.open("GET", url, true);
-        oReq.responseType = "arraybuffer";
+		reader.readAsArrayBuffer(fileReference);
+	}
 
-        oReq.onload = function (oEvent) {
-            var arrayBuffer = oReq.response; // Note: not oReq.responseText
-            bufferHandler(arrayBuffer);
-        };
+	function bufferFromUrl(url, bufferHandler) {
+		var oReq = new XMLHttpRequest();
+		oReq.open("GET", url, true);
+		oReq.responseType = "arraybuffer";
 
-        oReq.send(null);
-    }
-}
+		oReq.onload = function(oEvent) {
+			var arrayBuffer = oReq.response; // Note: not oReq.responseText
+			bufferHandler(arrayBuffer);
+		};
+
+		oReq.send(null);
+	}
+
+	var self = {
+		reader: reader,
+		bufferFromFileReference: bufferFromFileReference,
+		bufferFromUrl: bufferFromUrl
+	}
+
+	if (!window.FileManager) window.FileManager = self;
+
+})(window);
