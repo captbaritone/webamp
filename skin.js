9e41468ac16e7b740601844d4819d318a50ce4c1
PAEz <cdkpaez@gmail.com>
2014-12-04 03:43:49 +1030

    Skin
    When doing class/module/functional stuff its best practise (according to
    one of my teachers and makes good sense to me) that the
    class/module/function/woteva knows NOTHING of its surroundings.  Such
    things should be passed in through init or some public property.
    
    Ive set some variables with something like... zipper=window.JSZip, which
    isnt really cool.  It should really be passed in through the IIFEs
    arguments, or ideally (because of what I stated above) be passed in
    through init or a public property.
    
    Keep in mind I left an underscore on private functions.  This just made
    my life easier and it would be fine to change them.

--- skin.js
+++ skin.js
@@ -1,105 +1,122 @@
-// Dynamically set the css background images for all the sprites
-SkinManager = {
-    fileManager: FileManager,
-    font: Font,
-    init: function(styleNode, visualizerNode, analyser) {
-        this.styleNode = styleNode;
-        this.visualizer = Visualizer.init(visualizerNode, analyser);
-        return this;
-    },
+(function(window, undefined) {
+	/*
+    These two would be bad practice according to one of my teachers....
+    A class/module/woteva should never assume whats available and what its called
+    it should be passed in (through init, public property) and named internaly (same name is fine)
+    A class/module/woteva should be as singular as possible and know NOTHING of its surroundings
+    Also, with an IIFE you normally pass in stuff from the outsid with its arguments to make globals
+*/
+	var fileManager = window.FileManager;
+	var font = window.Font;
 
-    _skinImages: {
-        "#winamp": "MAIN.BMP",
-        "#title-bar": "TITLEBAR.BMP",
-        "#title-bar #option": "TITLEBAR.BMP",
-        "#title-bar #minimize": "TITLEBAR.BMP",
-        "#title-bar #shade": "TITLEBAR.BMP",
-        "#title-bar #close": "TITLEBAR.BMP",
-        ".status #clutter-bar": "TITLEBAR.BMP",
-        ".status #play-pause": "PLAYPAUS.BMP",
-        ".play .status #work-indicator": "PLAYPAUS.BMP",
-        ".status #time #minus-sign": "NUMBERS.BMP",
-        ".media-info .mono-stereo div": "MONOSTER.BMP",
-        "#volume": "VOLUME.BMP",
-        "#volume::-webkit-slider-thumb": "VOLUME.BMP",
-        "#volume::-moz-range-thumb": "VOLUME.BMP",
-        "#balance": "BALANCE.BMP",
-        "#balance::-webkit-slider-thumb": "VOLUME.BMP",
-        "#balance::-moz-range-thumb": "VOLUME.BMP",
-        ".windows div": "SHUFREP.BMP",
-        "#position": "POSBAR.BMP",
-        "#position::-webkit-slider-thumb": "POSBAR.BMP",
-        "#position::-moz-range-thumb": "POSBAR.BMP",
-        ".actions div": "CBUTTONS.BMP",
-        "#eject": "CBUTTONS.BMP",
-        ".shuffle-repeat div": "SHUFREP.BMP",
-        ".character": "TEXT.BMP",
-        ".digit": "NUMBERS.BMP",
-        // Put this second, since it will trump .digit
-        ".digit-ex": "NUMS_EX.BMP",
-        ".shade #position": "TITLEBAR.BMP",
-        ".shade #position::-webkit-slider-thumb": "TITLEBAR.BMP",
-        ".shade #position::-moz-range-thumb": "TITLEBAR.BMP",
-    },
+	function init(styleNode, visualizerNode, analyser) {
+		self.styleNode = styleNode;
+		self.visualizer = Visualizer.init(visualizerNode, analyser);
+		return self;
+	}
 
-    // Given a file of an original Winamp WSZ file, set the current skin
-    setSkinByFileReference: function(fileReference) {
-        this.fileManager.bufferFromFileReference(fileReference, this._setSkinByBuffer.bind(this));
-    },
+	var skinImages = {
+		"#winamp": "MAIN.BMP",
+		"#title-bar": "TITLEBAR.BMP",
+		"#title-bar #option": "TITLEBAR.BMP",
+		"#title-bar #minimize": "TITLEBAR.BMP",
+		"#title-bar #shade": "TITLEBAR.BMP",
+		"#title-bar #close": "TITLEBAR.BMP",
+		".status #clutter-bar": "TITLEBAR.BMP",
+		".status #play-pause": "PLAYPAUS.BMP",
+		".play .status #work-indicator": "PLAYPAUS.BMP",
+		".status #time #minus-sign": "NUMBERS.BMP",
+		".media-info .mono-stereo div": "MONOSTER.BMP",
+		"#volume": "VOLUME.BMP",
+		"#volume::-webkit-slider-thumb": "VOLUME.BMP",
+		"#volume::-moz-range-thumb": "VOLUME.BMP",
+		"#balance": "BALANCE.BMP",
+		"#balance::-webkit-slider-thumb": "VOLUME.BMP",
+		"#balance::-moz-range-thumb": "VOLUME.BMP",
+		".windows div": "SHUFREP.BMP",
+		"#position": "POSBAR.BMP",
+		"#position::-webkit-slider-thumb": "POSBAR.BMP",
+		"#position::-moz-range-thumb": "POSBAR.BMP",
+		".actions div": "CBUTTONS.BMP",
+		"#eject": "CBUTTONS.BMP",
+		".shuffle-repeat div": "SHUFREP.BMP",
+		".character": "TEXT.BMP",
+		".digit": "NUMBERS.BMP",
+		// Put this second, since it will trump .digit
+		".digit-ex": "NUMS_EX.BMP",
+		".shade #position": "TITLEBAR.BMP",
+		".shade #position::-webkit-slider-thumb": "TITLEBAR.BMP",
+		".shade #position::-moz-range-thumb": "TITLEBAR.BMP",
+	}
 
-    // Given the url of an original Winamp WSZ file, set the current skin
-    setSkinByUrl: function(url) {
-        this.fileManager.bufferFromUrl(url, this._setSkinByBuffer.bind(this));
-    },
+	// Given a file of an original Winamp WSZ file, set the current skin
+	function setSkinByFileReference(fileReference) {
+		fileManager.bufferFromFileReference(fileReference, _setSkinByBuffer);
+	}
 
-    // Given a bufferArray containing a Winamp WSZ file, set the current skin
-    // Gets passed as a callback, so don't have access to `this`
-    _setSkinByBuffer: function(buffer) {
-        var zip = new JSZip(buffer);
+	// Given the url of an original Winamp WSZ file, set the current skin
+	function setSkinByUrl(url) {
+		fileManager.bufferFromUrl(url, _setSkinByBuffer);
+	}
 
-        // XXX Ideally we would empty the style tag here, but I don't know how
-        // Appending overwrites, which has the same net effect, but after
-        // several skin changes, this tag will get pretty bloated.
-        var cssRules = '';
-        for(var selector in SkinManager._skinImages) {
-            var fileName = SkinManager._skinImages[selector];
-            var file = this._findFileInZip(fileName, zip);
+	// Given a bufferArray containing a Winamp WSZ file, set the current skin
+	// Gets passed as a callback, so don't have access to `this`
+	function _setSkinByBuffer(buffer) {
+		var zip = new window.JSZip(buffer); // prolly should pass in with IIFE arguments..but BETTER is pass it in init, read the comment block at the top
 
-            if (file) {
-                var value = "background-image: url(data:image/bmp;base64," + btoa(file.asBinary()) + ")"
-                cssRules += selector + "{" + value + "}\n";
-            }
+		// XXX Ideally we would empty the style tag here, but I don't know how
+		// Appending overwrites, which has the same net effect, but after
+		// several skin changes, this tag will get pretty bloated.
+		var cssRules = '';
+		for (var selector in skinImages) {
+			var fileName = skinImages[selector];
+			var file = _findFileInZip(fileName, zip);
 
-        }
+			if (file) {
+				var value = "background-image: url(data:image/bmp;base64," + btoa(file.asBinary()) + ")"
+				cssRules += selector + "{" + value + "}\n";
+			}
 
-        // Clear the loading state
-        document.getElementById('winamp').classList.remove('loading');
-        this.styleNode.appendChild(document.createTextNode(cssRules));
+		}
 
-        this._parseVisColors(zip);
+		// Clear the loading state
+		document.getElementById('winamp').classList.remove('loading');
+		self.styleNode.appendChild(document.createTextNode(cssRules));
 
-    },
+		_parseVisColors(zip);
+	}
 
-    _parseVisColors: function(zip) {
-        var entries = this._findFileInZip("VISCOLOR.TXT", zip).asText().split("\n");
-        var regex = /^(\d+)\s*,\s*(\d+)\s*,\s*(\d+)/
-        // changed to a hard number to deal with empty lines at the end...
-        // plus its only meant to be an exact amount of numbers anywayz
-        // - @PAEz
-        for(var i = 0; i < 24; i++) {
-            var matches = regex.exec(entries[i]);
-            if(matches) {
-                this.visualizer.colors[i] = 'rgb(' + matches.slice(1,4).join(',') + ')';
-            } else {
-                console.error('Error in VISCOLOR.TXT on line', i);
-                this.visColors.push('rgb(255,0,0)');
-            }
-        }
-    },
+	function _parseVisColors(zip) {
+		var entries = _findFileInZip("VISCOLOR.TXT", zip).asText().split("\n");
+		var regex = /^(\d+)\s*,\s*(\d+)\s*,\s*(\d+)/
+			// changed to a hard number to deal with empty lines at the end...
+			// plus its only meant to be an exact amount of numbers anywayz
+			// - @PAEz
+		for (var i = 0; i < 24; i++) {
+			var matches = regex.exec(entries[i]);
+			if (matches) {
+				self.visualizer.colors[i] = 'rgb(' + matches.slice(1, 4).join(',') + ')';
+			} else {
+				window.console.error('Error in VISCOLOR.TXT on line', i); // outside reference
+				visColors.push('rgb(255,0,0)');
+			}
+		}
+	}
 
-    _findFileInZip: function(name, zip) {
+    function _findFileInZip (name, zip) {
         return zip.filter(function (relativePath, file){
             return new RegExp("(^|/)" + name, 'i').test(relativePath)
         })[0];
     }
-}
+
+	var self = {
+        fileManager:FileManager,
+        font:Font,
+		init: init,
+		setSkinByFileReference: setSkinByFileReference,
+		setSkinByUrl: setSkinByUrl
+	}
+
+	if (!window.SkinManager) window.SkinManager = self;
+
+})(window);
