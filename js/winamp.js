78b58a5d4bb5e474077467431b0f05b995565485
Jordan Eldredge <jordaneldredge@gmail.com>
2014-12-20 23:45:10 -0800

    Abstract window management code
    In preparation of having multiple windows.
    
    Also remove analytics code that snuck in

--- js/winamp.js
+++ js/winamp.js
@@ -2,6 +2,7 @@
 Winamp = {
     init: function(options) {
         this.fileManager = FileManager;
+        this.windowManager = WindowManager;
         this.media = Media.init();
         this.skin = SkinManager.init(document.getElementById('visualizer'), this.media._analyser);
 
@@ -59,60 +60,8 @@ Winamp = {
 
     _registerListeners: function() {
         self = this;
-        // Make window dragable
-        this.nodes.titleBar.addEventListener('mousedown',function(e){
-            if(e.target !== this) {
-                // Prevent going into drag mode when clicking any of the title
-                // bar's icons by making sure the click was made directly on the
-                // titlebar
-                return true; }
 
-            // Get starting window position
-            var winampElm = self.nodes.winamp;
-
-            // If the element was 'absolutely' positioned we could simply use
-            // offsetLeft / offsetTop however the element is 'relatively'
-            // positioned so we're using style.left. parseInt is used to remove the
-            // 'px' postfix from the value
-            var winStartLeft = parseInt(winampElm.offsetLeft || 0,10),
-                winStartTop  = parseInt(winampElm.offsetTop || 0,10);
-
-            // Get starting mouse position
-            var mouseStartLeft = e.clientX,
-                mouseStartTop = e.clientY;
-
-            // Mouse move handler function while mouse is down
-            function handleMove(e) {
-                // Get current mouse position
-                var mouseLeft = e.clientX,
-                    mouseTop = e.clientY;
-
-                // Calculate difference offsets
-                var diffLeft = mouseLeft-mouseStartLeft,
-                    diffTop = mouseTop-mouseStartTop;
-
-                // These margins were only useful for centering the div, now we
-                // don't need them
-                winampElm.style.marginLeft = "0px";
-                winampElm.style.marginTop = "0px";
-                // Move window to new position
-                winampElm.style.left = (winStartLeft+diffLeft)+"px";
-                winampElm.style.top = (winStartTop+diffTop)+"px";
-            }
-
-            // Mouse button up
-            function handleUp() {
-                removeListeners();
-            }
-
-            function removeListeners() {
-                window.removeEventListener('mousemove',handleMove);
-                window.removeEventListener('mouseup',handleUp);
-            }
-
-            window.addEventListener('mousemove',handleMove);
-            window.addEventListener('mouseup',handleUp);
-        });
+        this.windowManager.registerWindow(this.nodes.winamp, this.nodes.titleBar);
 
         this.nodes.option.onclick = function(event) {
             event.stopPropagation();
