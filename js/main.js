b5c7824f86c7afd86b2b28f6e47897443da024a7
Jordan Eldredge <jordan@jordaneldredge.com>
2016-07-03 21:37:12 -0700

    Begin introducing es6

--- js/main.js
+++ js/main.js
@@ -1,41 +1,31 @@
-define([
-  'browser',
-  'main-window-dom',
-  '../css/winamp.css',
-  '../css/main-window.css',
-  '../css/context-menu.css',
-  'winamp',
-  'context',
-  'hotkeys'
-], function(
-  Browser,
-  mainWindowDom,
-  winampCss,
-  mainWindowCss,
-  contextMenuCss,
-  Winamp,
-  Context,
-  Hotkeys
-) {
-  if (new Browser(window).isCompatible) {
-    var mainWindowElement = document.createElement('div');
-    mainWindowElement.appendChild(mainWindowDom);
-    document.getElementById('winamp2-js').appendChild(mainWindowElement);
+import Browser from './browser';
+import mainWindowDom from './main-window-dom';
+import Winamp from './winamp';
+import Context from './context';
+import Hotkeys from './hotkeys';
 
-    var winamp = Winamp.init({
-      volume: 50,
-      balance: 0,
-      mediaFile: {
-        url: 'https://cdn.rawgit.com/captbaritone/llama/master/llama-2.91.mp3',
-        name: "1. DJ Mike Llama - Llama Whippin' Intro"
-      },
-      skinUrl: 'https://cdn.rawgit.com/captbaritone/winamp-skins/master/v2/base-2.91.wsz'
-    });
+import '../css/winamp.css';
+import '../css/main-window.css';
+import '../css/context-menu.css';
 
-    new Hotkeys(winamp);
-    new Context(winamp);
-  } else {
-    document.getElementById('winamp').style.display = 'none';
-    document.getElementById('browser-compatibility').style.display = 'block';
-  }
-});
+if (new Browser(window).isCompatible) {
+  var mainWindowElement = document.createElement('div');
+  mainWindowElement.appendChild(mainWindowDom);
+  document.getElementById('winamp2-js').appendChild(mainWindowElement);
+
+  var winamp = Winamp.init({
+    volume: 50,
+    balance: 0,
+    mediaFile: {
+      url: 'https://cdn.rawgit.com/captbaritone/llama/master/llama-2.91.mp3',
+      name: "1. DJ Mike Llama - Llama Whippin' Intro"
+    },
+    skinUrl: 'https://cdn.rawgit.com/captbaritone/winamp-skins/master/v2/base-2.91.wsz'
+  });
+
+  new Hotkeys(winamp);
+  new Context(winamp);
+} else {
+  document.getElementById('winamp').style.display = 'none';
+  document.getElementById('browser-compatibility').style.display = 'block';
+}
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,14 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['es2015']
+        }
       }
     ],
     noParse: [
