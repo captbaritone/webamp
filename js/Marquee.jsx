6b65e944e2224c2c4de51ce309b729ec0584cc54
Jordan Eldredge <jordan@jordaneldredge.com>
2016-07-10 19:07:36 -0700

    Remove rudunant constructor

--- js/Marquee.jsx
+++ js/Marquee.jsx
@@ -4,9 +4,6 @@ import {connect} from 'react-redux';
 
 
 class Marquee extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   componentDidMount() {
     const step = () => {
       setTimeout(() => {
