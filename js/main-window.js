d66dc86bd555aed413504938d0eb34186c399d6b
Jordan Eldredge <jordan@jordaneldredge.com>
2016-07-28 08:40:09 -0700

    Clean up main-window.js before final refactor

--- js/main-window.js
+++ js/main-window.js
@@ -1,24 +1,15 @@
 module.exports = {
   init: function(winamp) {
-    this.winamp = winamp;
     this.nodes = {
-      visualizer: document.getElementById('visualizer'),
-      workIndicator: document.getElementById('work-indicator'),
       window: document.getElementById('main-window')
     };
 
     this.handle = document.getElementById('title-bar');
     this.body = this.nodes.window;
 
-    this._registerListeners();
-    return this;
-  },
-
-  _registerListeners: function() {
-    var self = this;
-
-    this.nodes.visualizer.onclick = function() {
-      self.winamp.toggleVisualizer();
+    document.getElementById('visualizer').onclick = function() {
+      winamp.toggleVisualizer();
     };
+    return this;
   }
 };
