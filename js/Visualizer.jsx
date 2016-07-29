fd2b721c7441c4f80ba67c6521e1229944958bfe
Jordan Eldredge <jordan@jordaneldredge.com>
2016-07-28 18:09:32 -0700

    Move visualizer code out of main-window.js

---
+++ js/Visualizer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+class Visualizer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.dispatch({type: 'TOGGLE_VISUALIZER'});
+  }
+  render() {
+    return <canvas
+      onClick={this.handleClick}
+      id='visualizer'
+      width='152'
+      height='32'
+    />;
+  }
+}
+
+module.exports = connect(state => state)(Visualizer);
