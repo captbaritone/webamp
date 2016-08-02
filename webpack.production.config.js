517094fd4f26e960a7791c507f2288e6b377b524
Jordan Eldredge <jordan@jordaneldredge.com>
2016-08-01 21:57:04 -0700

    Separate webackconfig for prod (#183)

---
+++ webpack.production.config.js
@@ -0,0 +1,13 @@
+const config = require('./webpack.config');
+const webpack = require('webpack');
+
+config.plugins = (config.plugins || []).concat([
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin()
+]);
+
+module.exports = config;
