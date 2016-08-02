a5cbc6f848125b82c8452cfbb1a713723b91c41c
Jordan Eldredge <jordan@jordaneldredge.com>
2016-08-01 21:47:30 -0700

    Separate webackconfig for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,6 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ],
   module: {
     loaders: [
       {
