92b19c9ecfcb72f43f5acbbb27188d8e9217e32f
Jordan Eldredge <jordan@jordaneldredge.com>
2016-08-02 22:44:05 -0700

    Move SET_VOLUME to actionCreators

--- js/__tests__/utils-test.js
+++ js/__tests__/utils-test.js
@@ -3,12 +3,7 @@ jest.unmock('../utils');
 import {
   getTimeObj,
   getTimeStr,
-  getBalanceText,
-  getVolumeText,
-  getPositionText,
-  getMediaText,
-  getDoubleSizeModeText,
-  wrapForMarquee
+  clamp
 } from '../utils';
 
 describe('getTimeObj', () => {
@@ -38,6 +33,24 @@ describe('getTimeStr', () => {
   it('truncates extra minutes', () => {
     const actual = getTimeStr(540000);
     const expected = '9000:00';
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('clamp', () => {
+  it('respects the max value', () => {
+    const actual = clamp(101, 0, 100);
+    const expected = 100;
+    expect(actual).toEqual(expected);
+  });
+  it('respects the min value', () => {
+    const actual = clamp(0, 1, 100);
+    const expected = 1;
+    expect(actual).toEqual(expected);
+  });
+  it('respects the given value if in range', () => {
+    const actual = clamp(50, 0, 100);
+    const expected = 50;
     expect(actual).toEqual(expected);
   });
 });
