1f1162af8bcc36f48cb8bbb0d4223465a8031652
Jordan Eldredge <jordan@jordaneldredge.com>
2016-08-04 09:11:12 -0700

    Don't quote object properties that don't need to be quoted.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 config.plugins = (config.plugins || []).concat([
   new webpack.DefinePlugin({
     'process.env': {
-      'NODE_ENV': JSON.stringify('production')
+      NODE_ENV: JSON.stringify('production')
     }
   }),
   new webpack.optimize.UglifyJsPlugin()
