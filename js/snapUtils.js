785af977003f83fe9ca1a35fc3bc30bf0c81b23f
Jordan Eldredge <jordan@jordaneldredge.com>
2016-09-26 18:26:50 -0700

    Add snapWithin() and add snapping to like sides

--- js/snapUtils.js
+++ js/snapUtils.js
@@ -22,23 +22,28 @@ export const overlapY = (a, b) => {
 export const snap = (boxA, boxB) => {
   let x, y;
 
+  // TODO: Refactor/simplify this code
   if (overlapY(boxA, boxB)) {
     if (near(left(boxA), right(boxB))) {
       x = right(boxB);
-    }
-
-    if (near(right(boxA), left(boxB))) {
+    } else if (near(right(boxA), left(boxB))) {
       x = left(boxB) - boxA.width;
+    } else if (near(left(boxA), left(boxB))) {
+      x = left(boxB);
+    } else if (near(right(boxA), right(boxB))) {
+      x = right(boxB) - boxA.width;
     }
   }
 
   if (overlapX(boxA, boxB)) {
     if (near(top(boxA), bottom(boxB))) {
       y = bottom(boxB);
-    }
-
-    if (near(bottom(boxA), top(boxB))) {
+    } else if (near(bottom(boxA), top(boxB))) {
       y = top(boxB) - boxA.height;
+    } else if (near(top(boxA), top(boxB))) {
+      y = top(boxB);
+    } else if (near(bottom(boxA), bottom(boxB))) {
+      y = bottom(boxB) - boxA.height;
     }
   }
   return {x, y};
@@ -55,3 +60,29 @@ export const snapToMany = (boxA, otherBoxes) => {
 
   return {x, y};
 };
+
+export const snapWithin = (boxA, boundingBox) => {
+  let x, y;
+
+  if (boxA.x - SNAP_DISTANCE < 0) {
+    x = 0;
+  } else if ((boxA.x + boxA.width + SNAP_DISTANCE) > boundingBox.width) {
+    x = boundingBox.width - boxA.width;
+  }
+
+  if (boxA.y - SNAP_DISTANCE < 0) {
+    y = 0;
+  } else if ((boxA.y + boxA.height + SNAP_DISTANCE) > boundingBox.height) {
+    y = boundingBox.height - boxA.height;
+  }
+
+  return {x, y};
+};
+
+export const applySnap = (original, ...snaps) => (
+  snaps.reduce((previous, snapped) => ({
+    ...previous,
+    x: typeof snapped.x !== 'undefined' ? snapped.x : previous.x,
+    y: typeof snapped.y !== 'undefined' ? snapped.y : previous.y
+  }), original)
+);
