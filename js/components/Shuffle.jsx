8c635446a3d5ca5b98677357e6551ded5b03c425
Jordan Eldredge <jordan@jordaneldredge.com>
2016-12-29 21:59:49 -0800

    Convert Shuffle to mapDispatchToProps

--- js/components/Shuffle.jsx
+++ js/components/Shuffle.jsx
@@ -3,21 +3,15 @@ import {connect} from 'react-redux';
 import classnames from 'classnames';
 
 
-class Shuffle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.props.dispatch({type: 'TOGGLE_SHUFFLE'});
-  }
-  render() {
-    return <div
-      id='shuffle'
-      className={classnames({selected: this.props.shuffle})}
-      onClick={this.handleClick}
-    />;
-  }
-}
-
-module.exports = connect((state) => state.media)(Shuffle);
+const Shuffle = ({shuffle, toggleShuffle}) => (
+  <div
+    id='shuffle'
+    className={classnames({selected: shuffle})}
+    onClick={toggleShuffle}
+  />
+);
+const mapStateToProps = (state) => state.media;
+const mapDispatchToProps = (dispatch) => ({
+  toggleShuffle: () => dispatch({type: 'TOGGLE_SHUFFLE'})
+});
+module.exports = connect(mapStateToProps, mapDispatchToProps)(Shuffle);
