69cf900c0ba6561ae0c73ff1b88fa89718dcabda
Jordan Eldredge <jordan@jordaneldredge.com>
2017-01-13 21:11:12 -0800

    Add snapshot test for MainWindow

---
+++ js/__tests__/MainWindow.js
@@ -0,0 +1,46 @@
+const Winamp = ({
+  media: {
+    addEventListener: jest.fn(),
+    _analyser: null
+  }
+});
+
+const canvasMockify = require('canvas-mock');
+
+function createNodeMock(element) {
+  if (element.type === 'canvas') {
+    const Canvas = window.document.createElement('canvas');
+    canvasMockify(Canvas); // mock canvas functions required by Phaser.js are added
+    return Canvas;
+  }
+  return null;
+}
+
+window.requestAnimationFrame = () => {};
+
+import React from 'react';
+import {Provider} from 'react-redux';
+import renderer from 'react-test-renderer';
+import getStore from '../store';
+
+import MainWindow from '../components/MainWindow';
+
+describe('MainWindow', () => {
+  let store;
+  beforeEach(() => {
+    store = getStore(Winamp);
+  });
+
+  it('renders to snapshot', () => {
+    const options = {createNodeMock};
+    const tree = renderer.create(
+      <Provider store={store}>
+        <MainWindow
+          winamp={Winamp}
+          mediaPlayer={Winamp.media}
+        />
+      </Provider>,
+      options).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
