2aec4d68a442139ecd99a9e408b8cd13f7dd9e0e
Jordan Eldredge <jordan@jordaneldredge.com>
2017-03-20 22:49:23 +0000

    Add regression test playing local mp3s

---
+++ js/mediaMiddleware.test.js
@@ -0,0 +1,21 @@
+import mediaMiddleware from './mediaMiddleware';
+import {LOAD_AUDIO_FILE} from './actionTypes';
+
+
+const mockMedia = {
+  addEventListener: jest.fn(),
+  loadFromFileReference: jest.fn()
+};
+const mockStore = {}; 
+const mockNext = jest.fn();
+
+const middleware = mediaMiddleware(mockMedia)(mockStore)(mockNext);
+
+describe('Inbound actions', () => {
+  it('intercepts LOAD_AUDIO_FILE', () => {
+    const mockFile = {fileReference: {}};
+    middleware({type: LOAD_AUDIO_FILE, file: mockFile});
+    expect(mockMedia.loadFromFileReference)
+      .toHaveBeenCalledWith(mockFile.fileReference);
+  });
+});
