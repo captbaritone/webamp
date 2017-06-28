523dc58363dc13cf3c3ce117e822f7c9d2cc5e06
Jordan Eldredge <jordan@jordaneldredge.com>
2017-06-28 16:56:25 -0700

    Fix transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env"]
-          }
+          loader: "babel-loader"
         }
       }
     ],
