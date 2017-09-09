e8aff5c2cd8b85e2b3faab42afcfebcc3507caad
Jordan Eldredge <jordan@jordaneldredge.com>
2017-09-08 19:45:57 -0700

    Fix user input reducer tests

--- js/reducers.test.js
+++ js/reducers.test.js
@@ -2,26 +2,43 @@ import { userInput } from "./reducers";
 import { SET_FOCUS, SET_SCRUB_POSITION, UNSET_FOCUS } from "./actionTypes";
 
 describe("userInput reducer", () => {
-  const state = userInput();
+  const state = userInput(undefined, { type: "@@INIT" });
   it("has sensible defaults", () => {
-    expect(state).toEqual({ focus: null, scrubPosition: 0 });
+    expect(state).toEqual({ focus: null, scrubPosition: 0, bandFocused: null });
   });
   it("can set focus", () => {
-    const newState = userInput(state, { type: SET_FOCUS, input: "foo" });
-    expect(newState).toEqual({ focus: "foo", scrubPosition: 0 });
+    const newState = userInput(state, {
+      type: SET_FOCUS,
+      input: "foo",
+      bandFocused: null
+    });
+    expect(newState).toEqual({
+      focus: "foo",
+      scrubPosition: 0,
+      bandFocused: null
+    });
   });
   it("can unset focus", () => {
     const newState = userInput(
-      { ...state, focus: "foo" },
+      { ...state, focus: "foo", bandFocused: null },
       { type: UNSET_FOCUS }
     );
-    expect(newState).toEqual({ focus: null, scrubPosition: 0 });
+    expect(newState).toEqual({
+      focus: null,
+      scrubPosition: 0,
+      bandFocused: null
+    });
   });
   it("can set scrub position", () => {
     const newState = userInput(state, {
       type: SET_SCRUB_POSITION,
-      position: 5
+      position: 5,
+      bandFocused: null
     });
-    expect(newState).toEqual({ focus: null, scrubPosition: 5 });
+    expect(newState).toEqual({
+      focus: null,
+      scrubPosition: 5,
+      bandFocused: null
+    });
   });
 });
