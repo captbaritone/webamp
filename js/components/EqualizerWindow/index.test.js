26d7a43f62a871e8b3e7597c57b92814951b9a06
Jordan Eldredge <jordan@jordaneldredge.com>
2017-09-12 08:23:18 -0700

    Add snapshot of EQ window

---
+++ js/components/EqualizerWindow/index.test.js
@@ -0,0 +1,46 @@
+const Winamp = {
+  media: {
+    addEventListener: jest.fn(),
+    _analyser: null
+  }
+};
+
+const canvasMockify = require("canvas-mock");
+
+function createNodeMock(element) {
+  if (element.type === "canvas") {
+    const Canvas = window.document.createElement("canvas");
+    canvasMockify(Canvas); // mock canvas functions required by Phaser.js are added
+    return Canvas;
+  }
+  return null;
+}
+
+window.requestAnimationFrame = () => {};
+
+import React from "react";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+import getStore from "../../store";
+
+import EqualizerWindow from "./index";
+
+describe("EqualizerWindow", () => {
+  let store;
+  beforeEach(() => {
+    store = getStore(Winamp);
+  });
+
+  it("renders to snapshot", () => {
+    const options = { createNodeMock };
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <EqualizerWindow fileInput={null} />
+        </Provider>,
+        options
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
