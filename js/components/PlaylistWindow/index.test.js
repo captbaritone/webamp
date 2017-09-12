f0f381879c308f95f1156e82488d54f2c7054d21
Jordan Eldredge <jordan@jordaneldredge.com>
2017-09-12 08:30:46 -0700

    Add snapshot for Playlist window

---
+++ js/components/PlaylistWindow/index.test.js
@@ -0,0 +1,33 @@
+const Winamp = {
+  media: {
+    addEventListener: jest.fn(),
+    _analyser: null
+  }
+};
+
+window.requestAnimationFrame = () => {};
+
+import React from "react";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+import getStore from "../../store";
+
+import PlaylistWindow from "./index";
+
+describe("PlaylistWindow", () => {
+  let store;
+  beforeEach(() => {
+    store = getStore(Winamp);
+  });
+
+  it("renders to snapshot", () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <PlaylistWindow />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
