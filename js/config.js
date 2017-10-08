b4a388ed317a558322ad6f58e347b02cfd1a8fda
Jordan Eldredge <jordan@jordaneldredge.com>
2017-10-08 12:16:01 -0700

    Query string cache busting does not work

--- js/config.js
+++ js/config.js
@@ -14,10 +14,7 @@ const or = (value, fallback) => (value === undefined ? fallback : value);
 
 const assetBase = process.env.NODE_ENV === "production" ? cdnUrl : "";
 // Turn on the incomplete playlist window
-export const skinUrl = or(
-  config.skinUrl,
-  `${assetBase}skins/base-2.91.wsz?v=1`
-);
+export const skinUrl = or(config.skinUrl, `${assetBase}skins/base-2.91.wsz`);
 export const audioUrl = or(config.audioUrl, `${assetBase}mp3/llama-2.91.mp3`);
 export const playlistEnabled = config.playlist || false;
 export const noMarquee = config.noMarquee || false;
