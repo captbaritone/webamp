e357130e0292fac080747081aafcb66ba0bd2136
Jordan Eldredge <jordan@jordaneldredge.com>
2017-10-27 08:20:52 -0700

    Check for Promise support
    Fixes: https://sentry.io/share/issue/b9bbf4221e3e48f0906b6d57dbfde1a8/

--- js/browser.js
+++ js/browser.js
@@ -1,6 +1,7 @@
 export default function(base) {
   const supportsAudioApi = !!(base.AudioContext || base.webkitAudioContext);
   const supportsCanvas = !!base.document.createElement("canvas").getContext;
+  const supportsPromises = typeof Promise !== "undefined";
 
-  this.isCompatible = supportsAudioApi && supportsCanvas;
+  this.isCompatible = supportsAudioApi && supportsCanvas && supportsPromises;
 }
