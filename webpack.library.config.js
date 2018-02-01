96a51eb245c5064b485b22ae77cee57e52e390ea
Jordan Eldredge <jordan@jordaneldredge.com>
2018-02-01 06:54:10 -0800

    Revert "Remove unused "library" experiment"
    This reverts commit a08ed96ab58ee7acf839496ad08f9e7a29817c0c.

---
+++ webpack.library.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(wsz|mp3)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              emitFile: true,
+              name: "[path][name]-[hash].[ext]"
+            }
+          }
+        ]
+      }
+    ],
+    noParse: [/jszip\.js$/]
+  },
+  entry: ["babel-polyfill", "./js/winamp.js"],
+  output: {
+    path: path.resolve(__dirname, "built"),
+    filename: "winamp.bundle.js",
+    library: "winamp2js",
+    libraryTarget: "umd"
+  }
+};
