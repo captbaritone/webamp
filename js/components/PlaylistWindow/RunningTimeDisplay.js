df81666b1d78252bed2a985cff2e58e841aaf10c
Jordan Eldredge <jordan@jordaneldredge.com>
2018-01-31 21:12:35 -0800

    Poor man's rightPad

--- js/components/PlaylistWindow/RunningTimeDisplay.js
+++ js/components/PlaylistWindow/RunningTimeDisplay.js
@@ -4,10 +4,20 @@ import { connect } from "react-redux";
 import CharacterString from "../CharacterString";
 import { getRunningTimeMessage } from "../../selectors";
 
+// While all the browsers I care about support String.prototype.padEnd,
+// Not all node versions do, and I want tests to pass in Jest...
+// Sigh.
+function rightPad(str, len, fillChar) {
+  while (str.length < len) {
+    str += fillChar;
+  }
+  return str;
+}
+
 const RunningTimeDisplay = props => (
   <div className="playlist-running-time-display draggable">
     <CharacterString>
-      {props.runningTimeMessage.padEnd(18, " ")}
+      {rightPad(props.runningTimeMessage, 18, " ")}
     </CharacterString>
   </div>
 );
