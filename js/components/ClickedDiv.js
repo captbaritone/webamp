6f98d28309064d7379558ad49d18cff6df39dea5
Jordan Eldredge <jordan@jordaneldredge.com>
2018-02-03 12:06:53 -0800

    Add comment to ClickedDiv

--- js/components/ClickedDiv.js
+++ js/components/ClickedDiv.js
@@ -1,6 +1,12 @@
 import React from "react";
 import classnames from "classnames";
 
+// Winamp has a strange behavior for the buttons at the top of the main window.
+// It shows through to the main background sprite until the first time that it's
+// clicked, and then it shows the dedicated undepressed sprite thereafter.
+// This component is an abstraction that tracks if a div has ever been clicked.
+// Look in `skinSelectors` for CSS selectors that look like `#some-id.clicked`
+// for examples of this functionality in use.
 export default class ClickedDiv extends React.Component {
   constructor(props) {
     super(props);
