95a1cfa09bf834f1a16c5bd9fef9cda6564a4aa8
Ricky Miller <ricky.miller@gmail.com>
2018-02-12 09:19:12 -0600

    Replace quotes

--- js/utils.js
+++ js/utils.js
@@ -78,7 +78,7 @@ export const parseIni = text => {
   let section, match;
   return text.split(/[\r\n]+/g).reduce((data, line) => {
     if ((match = line.match(PROPERTY_REGEX)) && section != null) {
-      data[section][match[1].trim().toLowerCase()] = match[2];
+      data[section][match[1].trim().toLowerCase()] = match[2].replace(/("|')/, '');
     } else if ((match = line.match(SECTION_REGEX))) {
       section = match[1].trim().toLowerCase();
       data[section] = {};
