95cad890e9d1a71a6d6507d967e652fa9fa05040
Jordan Eldredge <jordan@jordaneldredge.com>
2018-03-16 21:55:29 -0700

    Add dummy service-worker.js

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const merge = require("webpack-merge");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
@@ -9,6 +10,9 @@ module.exports = merge(common, {
       SENTRY_DSN: JSON.stringify(
         "https://c8c64ef822f54240901bc64f88c234d8@sentry.io/146022"
       )
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: "js/dev-service-worker.js", to: "service-worker.js", force: true }
+    ])
   ]
 });
---
+++ js/dev-service-worker.js
@@ -0,0 +1,1 @@
+// Just a dummy service worker file. In the prod build, this would do something real.
