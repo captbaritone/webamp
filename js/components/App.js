8758d688d743337875b5571f3557e60271ab6100
Jordan Eldredge <jordan@jordaneldredge.com>
2018-06-01 20:08:15 -0700

    Refactor App component

--- js/components/App.js
+++ js/components/App.js
@@ -13,64 +13,69 @@ import Skin from "./Skin";
 
 import "../../css/webamp.css";
 
-const App = ({
-  media,
-  closed,
-  genWindowsInfo,
-  container,
-  filePickers,
-  genWindowComponents
-}) => {
-  if (closed) {
-    return null;
+class App extends React.Component {
+  _renderWindows() {
+    const {
+      media,
+      genWindowsInfo,
+      filePickers,
+      genWindowComponents
+    } = this.props;
+    return objectMap(genWindowsInfo, (w, id) => {
+      if (!w.open) {
+        return null;
+      }
+      switch (id) {
+        case "main":
+          return (
+            <MainWindow
+              analyser={media.getAnalyser()}
+              filePickers={filePickers}
+            />
+          );
+        case "equalizer":
+          return <EqualizerWindow />;
+        case "playlist":
+          return <PlaylistWindow analyser={media.getAnalyser()} />;
+        default:
+          if (!w.generic) {
+            throw new Error("Tried to render an unknown window:", id);
+          }
+          const Component = genWindowComponents[id];
+          return (
+            <GenWindow title={w.title} windowId={id}>
+              {({ height, width }) => (
+                <Component
+                  analyser={media.getAnalyser()}
+                  width={width}
+                  height={height}
+                />
+              )}
+            </GenWindow>
+          );
+      }
+    });
   }
-
-  const windows = objectMap(genWindowsInfo, (w, id) => {
-    if (!w.open) {
+  render() {
+    const { closed, container, filePickers } = this.props;
+    if (closed) {
       return null;
     }
-    switch (id) {
-      case "main":
-        return (
-          <MainWindow
-            analyser={media.getAnalyser()}
-            filePickers={filePickers}
+    return (
+      <div role="application" id="webamp">
+        <Skin />
+        <ContextMenuWrapper
+          renderContents={() => <MainContextMenu filePickers={filePickers} />}
+        >
+          <WindowManager
+            windows={this._renderWindows()}
+            container={container}
           />
-        );
-      case "equalizer":
-        return <EqualizerWindow />;
-      case "playlist":
-        return <PlaylistWindow analyser={media.getAnalyser()} />;
-      default:
-        if (!w.generic) {
-          throw new Error("Tried to render an unknown window:", id);
-        }
-        const Component = genWindowComponents[id];
-        return (
-          <GenWindow title={w.title} windowId={id}>
-            {({ height, width }) => (
-              <Component
-                analyser={media.getAnalyser()}
-                width={width}
-                height={height}
-              />
-            )}
-          </GenWindow>
-        );
-    }
-  });
-
-  return (
-    <div role="application" id="webamp">
-      <Skin />
-      <ContextMenuWrapper
-        renderContents={() => <MainContextMenu filePickers={filePickers} />}
-      >
-        <WindowManager windows={windows} container={container} />
-      </ContextMenuWrapper>
-    </div>
-  );
-};
+        </ContextMenuWrapper>
+      </div>
+    );
+  }
+}
 
 App.propTypes = {
   container: PropTypes.instanceOf(Element)
