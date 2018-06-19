748cd7052029a2e1fb511a3e285f58f562815256
Jordan Eldredge <jordan@jordaneldredge.com>
2018-06-18 22:14:44 -0700

    Add portal for Milkdrop Desktop mode

---
+++ js/components/MilkdropWindow/Desktop.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class Desktop extends React.Component {
+  componentWillUnmount() {
+    document.body.removeChild(this._desktopNode);
+    this._desktopNode = null;
+  }
+
+  _getNode() {
+    if (this._desktopNode == null) {
+      this._desktopNode = document.createElement("div");
+      this._desktopNode.classList.add("webamp-desktop");
+      document.body.appendChild(this._desktopNode);
+    }
+    return this._desktopNode;
+  }
+
+  render() {
+    return ReactDOM.createPortal(this.props.children, this._getNode());
+  }
+}
