3d6f41e68dff9b65b0742eb072dc95a2f0ec540a
Jordan Eldredge <jordan@jordaneldredge.com>
2018-06-22 00:08:00 -0700

    Make exports explicit

--- js/actionCreators/index.js
+++ js/actionCreators/index.js
@@ -1,10 +1,70 @@
 import { CLOSE_WINAMP, STOP, TOGGLE_VISUALIZER_STYLE } from "../actionTypes";
 
-export * from "./windows";
-export * from "./media";
-export * from "./equalizer";
-export * from "./files";
-export * from "./playlist";
+export {
+  toggleDoubleSizeMode,
+  toggleEqualizerShadeMode,
+  togglePlaylistShadeMode,
+  closeWindow,
+  setWindowSize,
+  toggleWindow,
+  updateWindowPositions,
+  toggleMainWindowShadeMode
+} from "./windows";
+export {
+  play,
+  pause,
+  stop,
+  nextN,
+  next,
+  previous,
+  seekForward,
+  seekBackward,
+  setVolume,
+  adjustVolume,
+  scrollVolume,
+  setBalance,
+  toggleRepeat,
+  toggleShuffle
+} from "./media";
+export {
+  setEqBand,
+  setEqToMax,
+  setEqToMid,
+  setEqToMin,
+  setPreamp,
+  toggleEq
+} from "./equalizer";
+export {
+  addTracksFromReferences,
+  loadFilesFromReferences,
+  setSkinFromArrayBuffer,
+  setSkinFromFileReference,
+  setSkinFromUrl,
+  openEqfFileDialog,
+  openMediaFileDialog,
+  openSkinFileDialog,
+  fetchMediaDuration,
+  loadMediaFiles,
+  loadMediaFile,
+  fetchMediaTags,
+  setEqFromFileReference,
+  downloadPreset,
+  downloadHtmlPlaylist
+} from "./files";
+export {
+  cropPlaylist,
+  removeSelectedTracks,
+  removeAllTracks,
+  reverseList,
+  randomizeList,
+  sortListByTitle,
+  setPlaylistScrollPosition,
+  scrollNTracks,
+  scrollPlaylistByDelta,
+  scrollUpFourTracks,
+  scrollDownFourTracks,
+  dragSelected
+} from "./playlist";
 
 export function close() {
   return dispatch => {
