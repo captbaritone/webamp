179bab89fe177d7c0fe2e7816619316903c4a0b0
Jordan Eldredge <jordan@jordaneldredge.com>
2018-06-28 23:24:25 -0700

    Add more integration tests

--- js/__tests__/baseline.integration-test.js
+++ js/__tests__/baseline.integration-test.js
@@ -2,16 +2,31 @@ const { toMatchImageSnapshot } = require("jest-image-snapshot");
 
 expect.extend({ toMatchImageSnapshot });
 
-beforeAll(async () => {
-  await page.goto(`http://localhost:8080/#{"disableMarquee":true}`);
-});
+// Hack to ensure changing the hash causes a page reload
+beforeEach(async () => page.goto(`http://example.com`));
 
 it("should render the default skin", async () => {
+  await page.goto(`http://localhost:8080/#{"disableMarquee":true}`);
+  expect(await page.screenshot()).toMatchImageSnapshot();
+});
+
+it("can 'pose' for a screenshot", async () => {
+  await page.goto(
+    `http://localhost:8080/?screenshot=1#{"disableMarquee":true}`
+  );
   expect(await page.screenshot()).toMatchImageSnapshot();
 });
 
 it("should render the Topaz skin", async () => {
+  await page.goto(`http://localhost:8080/#{"disableMarquee":true}`);
   expect(page).toUploadFile("#webamp-file-input", "./skins/TopazAmp1-2.wsz");
+  await new Promise(resolve => setTimeout(resolve, 200));
+  expect(await page.screenshot()).toMatchImageSnapshot();
+});
+
+it("uses the volume spirtes as a fallback when balance spirtes are missing", async () => {
+  await page.goto(`http://localhost:8080/#{"disableMarquee":true}`);
+  expect(page).toUploadFile("#webamp-file-input", "./skins/AmigaPPC-dark.wsz");
   await new Promise(resolve => setTimeout(resolve, 200));
   expect(await page.screenshot()).toMatchImageSnapshot();
 });
