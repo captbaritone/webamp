2911f5214ecbd20e05e365c29f0394336c3a1e1e
Jordan Eldredge <jordan@jordaneldredge.com>
2018-07-11 13:25:12 -0700

    Improve screenshots

--- experiments/automatedScreenshots/index.js
+++ experiments/automatedScreenshots/index.js
@@ -1,129 +1,44 @@
-const defaultTracksState = {
-  "0": {
-    selected: false,
-    title: "Llama Whipping Intro",
-    artist: "DJ Mike Llama",
-    duration: "5",
-    url: ""
-  },
-  "1": {
-    selected: false,
-    title: "Rock Is Dead",
-    artist: "Marilyn Manson",
-    duration: "191", // 3:11
-    url: ""
-  },
-  "2": {
-    selected: true,
-    title: "Spybreak! (Short One)",
-    artist: "Propellerheads",
-    duration: "240", // 4:00
-    url: ""
-  },
-  "3": {
-    selected: false,
-    title: "Bad Blood",
-    artist: "Ministry",
-    duration: "300", // 5:00
-    url: ""
-  }
-};
-
 const fs = require("fs");
-const puppeteer = require("puppeteer");
+const path = require("path");
 const Filehound = require("filehound");
-const JSZip = require("jszip");
 const md5File = require("md5-file");
-
-const validateZip = u =>
-  new Promise((resolve, reject) => {
-    fs.readFile(u, (err, buffer) => {
-      JSZip.loadAsync(buffer).then(resolve, reject);
-    });
-  });
-
-const config = {
-  playlist: true,
-  hideAbout: true,
-  audioUrl: null,
-  initialState: {
-    equalizer: {
-      sliders: {
-        "60": 52,
-        "170": 74,
-        "310": 83,
-        "600": 91,
-        "1000": 74,
-        "3000": 54,
-        "6000": 23,
-        "12000": 19,
-        "14000": 34,
-        "16000": 75,
-        preamp: 56
-      }
-    },
-    media: {
-      status: "PLAYING",
-      kbps: 128,
-      khz: 44,
-      length: 5,
-      timeElapsed: 1,
-      channels: 2,
-      name: "1. DJ Mike Llama - Llama Whippin' Intro"
-    },
-    playlist: {
-      tracks: defaultTracksState,
-      trackOrder: [0, 1, 2, 3],
-      currentTrack: 0
-    },
-    display: {
-      working: false
-    }
-  }
-};
+const Shooter = require("./shooter");
 
 (async () => {
-  const files = await Filehound.create()
-    .ext("wsz")
-    .paths("skins/")
-    .find();
-
-  const browser = await puppeteer.launch();
+  const shooter = new Shooter();
+  const passedSkin = process.argv[2];
+  let files = [];
+  if (passedSkin) {
+    files.push(passedSkin);
+  } else {
+    files = await Filehound.create()
+      .ext("wsz")
+      .paths("skins/")
+      .find();
+  }
 
   for (const skin of files) {
+    console.log('Trying', skin);
     const skinMd5 = md5File.sync(skin);
-    const fileName = `${skin.replace(/\//g, "-")}-${skinMd5}`;
-    const screenshotFile = `screenshots/${fileName}.png`;
-    if (fs.existsSync(screenshotFile)) {
-      console.log(screenshotFile, "exists already");
-      continue;
-    }
+    const fileName = `${path.basename(skin).replace(/\//g, "-")}-${skinMd5}`;
+    const screenshotPath = `screenshots/${skinMd5}.png`;
     const flatSkinFile = `flatSkins/${fileName}.wsz`;
     if (!fs.existsSync(flatSkinFile)) {
       fs.linkSync(skin, flatSkinFile);
     }
-    const skinUrl = `experiments/automatedScreenshots/${skin}`;
-    console.log("Going to try", screenshotFile);
-    try {
-      await validateZip(`./${skin}`);
-    } catch (e) {
-      console.log("Error parsing", skinUrl, e);
+    const md5SkinFile = `md5Skins/${skinMd5}.wsz`;
+    if (!fs.existsSync(md5SkinFile)) {
+      console.log("Linked skin from", skin, "to", md5SkinFile);
+      fs.linkSync(skin, md5SkinFile);
+    }
+    if (fs.existsSync(screenshotPath)) {
+      console.log(screenshotPath, "exists already");
       continue;
     }
-    const page = await browser.newPage();
-    page.setViewport({ width: 275, height: 116 * 3 });
-    page.on("console", (...args) => {
-      console.log("PAGE LOG:", ...args);
+    await shooter.takeScreenshot(path.join(__dirname, skin), screenshotPath, {
+      minify: true
     });
-    config.skinUrl = skinUrl;
-    const url = `http://localhost:8080/#${JSON.stringify(config)}`;
-    console.log({ url });
-    await page.goto(url);
-    await page.waitForSelector("#main-window", { timeout: 2000 });
-
-    console.log("Writing screenshot to", screenshotFile);
-    await page.screenshot({ path: screenshotFile });
-    await page.close();
   }
-  browser.close();
+
+  await shooter.dispose();
 })();
---
+++ experiments/automatedScreenshots/shooter.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const JSZip = require("jszip");
+const imagemin = require("imagemin");
+const imageminOptipng = require("imagemin-optipng");
+
+function min(imgPath) {
+  return imagemin([imgPath], path.dirname(imgPath), {
+    use: [imageminOptipng()]
+  });
+}
+
+class Shooter {
+  constructor() {
+    this._initialized = false;
+  }
+
+  async init() {
+    this._browser = await puppeteer.launch();
+    this._page = await this._browser.newPage();
+    this._page.setViewport({ width: 275, height: 116 * 3 });
+    this._page.on("console", (...args) => {
+      console.log("PAGE LOG:", ...args);
+    });
+    const url = `http://localhost:8080/?screenshot=1`;
+    await this._page.goto(url);
+    await this._page.waitForSelector("#main-window", { timeout: 2000 });
+    await this._page.evaluate(() => {
+      // Needed to allow for transparent screenshots
+      window.document.body.style.background = "none";
+    });
+    this._initialized = true;
+  }
+
+  _validateZip(u) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(u, (err, buffer) => {
+        JSZip.loadAsync(buffer).then(resolve, reject);
+      });
+    });
+  }
+
+  async _ensureInitialized() {
+    if (!this._initialized) {
+      await this.init();
+    }
+  }
+
+  async takeScreenshot(skin, screenshotPath, { minify = false }) {
+    await this._ensureInitialized();
+    console.log("Going to try", screenshotPath);
+    try {
+      await this._validateZip(skin);
+    } catch (e) {
+      console.log("Error parsing", skin, e);
+      return;
+    }
+    try {
+      const handle = await this._page.$("#webamp-file-input");
+      await handle.uploadFile(skin);
+      await this._page.evaluate(() => {
+        return window.__webamp.skinIsLoaded();
+      });
+      await this._page.screenshot({
+        path: screenshotPath,
+        omitBackground: true // Make screenshot transparent
+      });
+      console.log("Wrote screenshot to", screenshotPath);
+      if (minify) {
+        min(screenshotPath);
+      }
+      console.log("Minified", screenshotPath);
+    } catch (e) {
+      console.error("Something went wrong, restarting browser", e);
+      await this.init();
+      throw e;
+    }
+  }
+
+  dispose() {
+    this._ensureInitialized();
+    this._browser.close();
+    this._initialized = false;
+  }
+}
+
+module.exports = Shooter;
