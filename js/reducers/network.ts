a276ffb5185a529daab6cabd47e0587bb4a7cedb
Jordan Eldredge <jordan@jordaneldredge.com>
2018-09-04 20:26:11 -0700

    Convert first files to TypeScript

--- js/reducers/network.ts
+++ js/reducers/network.ts
@@ -1,6 +1,8 @@
+import {Action, NetworkState} from "../types";
 import { NETWORK_CONNECTED, NETWORK_DISCONNECTED } from "../actionTypes";
 
-const network = (state = { connected: true }, action) => {
+
+const network = (state: NetworkState = { connected: true }, action: Action): NetworkState => {
   switch (action.type) {
     case NETWORK_CONNECTED:
       return { ...state, connected: true };
--- js/reducers/settings.ts
+++ js/reducers/settings.ts
@@ -1,10 +1,15 @@
+import { Action, SettingsState } from "../types";
 import { SET_AVAILABLE_SKINS } from "../actionTypes";
+
 
 const defaultSettingsState = {
   availableSkins: []
 };
 
-const settings = (state = defaultSettingsState, action) => {
+const settings = (
+  state: SettingsState = defaultSettingsState,
+  action: Action
+): SettingsState => {
   switch (action.type) {
     case SET_AVAILABLE_SKINS:
       return { ...state, availableSkins: action.skins };
