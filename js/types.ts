b2359c6b5a791c6d16ecafe97cc13876a8341e4c
Jordan Eldredge <jordan@jordaneldredge.com>
2018-09-04 21:53:56 -0700

    Convert media reducer to Typescript

--- js/types.ts
+++ js/types.ts
@@ -13,6 +13,48 @@ export type Action =
   | {
       type: "SET_AVAILABLE_SKINS";
       skins: Array<Skin>;
+  }
+  | {
+      type: "PLAY";
+  }
+  | {
+      type: "IS_PLAYING";
+  }
+  | {
+      type: "PAUSE";
+  }
+  | {
+      type: "STOP";
+  }
+  | {
+      type: "IS_STOPPED";
+  }
+  | {
+      type: "CHANNEL_COUNT_CHANGED";
+      channels: number;
+  } | {
+      type: "TOGGLE_TIME_MODE";
+  } | {
+      type: "UPDATE_TIME_ELAPSED";
+      elapsed: number;
+  } | {
+      type: "ADD_TRACK_FROM_URL";
+  } | {
+      type: "SET_MEDIA";
+      length: number;
+      kbps: number;
+      khz: number;
+      channels: number;
+  } | {
+      type: "SET_VOLUME";
+      volume: number;
+  } | {
+      type: "SET_BALANCE";
+      balance: number;
+  } | {
+      type: "TOGGLE_REPEAT";
+  } | {
+      type: "TOGGLE_SHUFFLE";
     };
 
 export interface SettingsState {
@@ -21,4 +63,18 @@ export interface SettingsState {
 
 export interface NetworkState {
   connected: boolean;
+}
+
+export interface MediaState {
+  timeMode: string; // TODO: Convert this to an enum
+  timeElapsed: number;
+  length: number | null;
+  kbps: number| null;
+  khz: number | null;
+  volume: number;
+  balance: number;
+  channels: number | null; // TODO: Convert this to an enum
+  shuffle: boolean;
+  repeat: boolean;
+  status: string | null; // TODO: Convert this to an enum
 }
