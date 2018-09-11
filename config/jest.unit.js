1af57575d3f7a0ef63e04bcac231b38b1868182a
Jordan Eldredge <jordan@jordaneldredge.com>
2018-09-11 16:54:32 -0700

    Allow Jest to process .ts files

--- config/jest.unit.js
+++ config/jest.unit.js
@@ -5,9 +5,13 @@ module.exports = {
   globals: {
     SENTRY_DSN: null
   },
+  moduleFileExtensions: ["js", "ts"],
   moduleNameMapper: {
     "\\.css$": "<rootDir>/js/__mocks__/styleMock.js",
     "\\.wsz$": "<rootDir>/js/__mocks__/fileMock.js",
     "\\.mp3$": "<rootDir>/js/__mocks__/fileMock.js"
+  },
+  transform: {
+    "^.+\\.(js|ts)$": "babel-jest"
   }
 };
