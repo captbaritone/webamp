6d6776a5b9d2e655bcdb1f53e6d2df371aa7e945
Jordan Eldredge <jordan@jordaneldredge.com>
2018-09-17 08:32:52 -0700

    Type passthrough props

--- js/components/Character.tsx
+++ js/components/Character.tsx
@@ -11,7 +11,16 @@ export const characterClassName = (char: string | number) =>
     .toLowerCase()
     .charCodeAt(0)}`;
 
-const Character = ({ children: char, className, ...passThrough }: Props) => (
+const Character = ({
+  children: char,
+  className,
+  ...passThrough
+}: Props &
+  // TODO: Seems like there should be a better way to do pass through props
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  >) => (
   <span
     {...passThrough}
     className={`${className || ""} character ${characterClassName(char)}`}
