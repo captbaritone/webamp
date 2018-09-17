bc7766efe1cdef664e106c7a815a108538fafb8e
Jordan Eldredge <jordan@jordaneldredge.com>
2018-09-17 08:32:52 -0700

    Teach ESLint and Jest about .tsx

--- config/jest.unit.js
+++ config/jest.unit.js
@@ -1,17 +1,17 @@
 module.exports = {
   displayName: "test",
   rootDir: "../",
-  testRegex: "\\.test\\.js$",
+  testRegex: "\\.test\\.(js|ts|tsx)$",
   globals: {
     SENTRY_DSN: null
   },
-  moduleFileExtensions: ["js", "ts"],
+  moduleFileExtensions: ["js", "tsx", "ts"],
   moduleNameMapper: {
     "\\.css$": "<rootDir>/js/__mocks__/styleMock.js",
     "\\.wsz$": "<rootDir>/js/__mocks__/fileMock.js",
     "\\.mp3$": "<rootDir>/js/__mocks__/fileMock.js"
   },
   transform: {
-    "^.+\\.(js|ts)$": "babel-jest"
+    "^.+\\.(js|ts|tsx)$": "babel-jest"
   }
 };
