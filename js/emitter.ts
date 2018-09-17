b4a48d57b8e8bc97fb3bb1942c11ad71c9558ca9
Jordan Eldredge <jordan@jordaneldredge.com>
2018-09-17 08:32:52 -0700

    Type Emitter

--- js/emitter.ts
+++ js/emitter.ts
@@ -1,5 +1,11 @@
 export default class Emitter {
-  on(event, callback) {
+  _listeners: { [event: string]: Array<(...args: any[]) => void> };
+
+  constructor() {
+    this._listeners = {};
+  }
+
+  on(event: string, callback: (...args: any[]) => void) {
     const eventListeners = this._listeners[event] || [];
     eventListeners.push(callback);
     this._listeners[event] = eventListeners;
@@ -9,14 +15,10 @@ export default class Emitter {
     return unsubscribe;
   }
 
-  trigger(event, ...args) {
+  trigger(event: string, ...args: any[]) {
     const callbacks = this._listeners[event];
     if (callbacks) {
       callbacks.forEach(cb => cb(...args));
     }
-  }
-
-  constructor() {
-    this._listeners = {};
   }
 }
