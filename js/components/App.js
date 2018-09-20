3f1861d4f83cc8a00c80121aa26cb3b6bcbf2272
Jordan Eldredge <jordan@jordaneldredge.com>
2018-09-19 23:52:55 -0700

    Persist focus, and handle bad focus

--- js/components/App.js
+++ js/components/App.js
@@ -52,7 +52,7 @@ class App extends React.Component {
 
   _setFocus() {
     const binding = this._bindings[this.props.focused];
-    if (binding.node) {
+    if (binding && binding.node) {
       binding.node.focus();
     }
   }
