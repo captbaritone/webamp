d9c559ea06ae25f09463d6483e1c634842d70367
Jordan Eldredge <jordan@jordaneldredge.com>
2018-09-24 17:05:12 -0700

    Clean up selectors file

--- js/selectors.ts
+++ js/selectors.ts
@@ -422,11 +422,9 @@ export function getEqualizerEnabled(state: AppState): boolean {
 export function getCenterRequested(state: AppState): boolean {
   return state.windows.centerRequested;
 }
-}
 
 export function getBrowserWindowSize(
   state: AppState
 ): { height: number; width: number } {
   return state.windows.browserWindowSize;
 }
-
