61a7b1ccda83bb712490f3dbc3315fa18692999c
Jordan Eldredge <jordan@jordaneldredge.com>
2018-10-10 20:34:41 -0700

    Assume we alays have a documentElement

--- js/utils.ts
+++ js/utils.ts
@@ -351,19 +351,19 @@ export function getWindowSize(): { width: number; height: number } {
   return {
     width: Math.max(
       document.body.scrollWidth,
-      document.documentElement.scrollWidth,
+      document.documentElement!.scrollWidth,
       document.body.offsetWidth,
-      document.documentElement.offsetWidth,
+      document.documentElement!.offsetWidth,
       document.body.clientWidth,
-      document.documentElement.clientWidth
+      document.documentElement!.clientWidth
     ),
     height: Math.max(
       document.body.scrollHeight,
-      document.documentElement.scrollHeight,
+      document.documentElement!.scrollHeight,
       document.body.offsetHeight,
-      document.documentElement.offsetHeight,
+      document.documentElement!.offsetHeight,
       document.body.clientHeight,
-      document.documentElement.clientHeight
+      document.documentElement!.clientHeight
     )
   };
 }
