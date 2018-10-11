34b93df7660f35ab3050c656dd0c3818280624ee
Jordan Eldredge <jordan@jordaneldredge.com>
2018-10-10 21:04:49 -0700

    Type PlaylistResizeTarget

---
+++ js/components/PlaylistWindow/PlaylistResizeTarget.tsx
@@ -0,0 +1,32 @@
+import { connect } from "react-redux";
+import ResizeTarget from "../ResizeTarget";
+import { setWindowSize } from "../../actionCreators";
+import { getWindowSize } from "../../selectors";
+import { AppState, Dispatch } from "../../types";
+
+interface StateProps {
+  currentSize: [number, number];
+  id: string;
+}
+
+interface DispatchProps {
+  currentSize: [number, number];
+  id: string;
+}
+
+const mapStateToProps = (state: AppState): StateProps => ({
+  currentSize: getWindowSize(state)("playlist"),
+  id: "playlist-resize-target"
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    setWindowSize: (size: [number, number]) =>
+      dispatch(setWindowSize("playlist", size))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ResizeTarget);
