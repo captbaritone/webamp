438ca38c5cd7e83ecb020e5e11d74a30a2a2cb4d
Jordan Eldredge <jordan@jordaneldredge.com>
2018-10-10 20:34:41 -0700

    Type SelectionMenu

--- js/components/PlaylistWindow/SelectionMenu.tsx
+++ js/components/PlaylistWindow/SelectionMenu.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import { SELECT_ALL, SELECT_ZERO, INVERT_SELECTION } from "../../actionTypes";
 import PlaylistMenu from "./PlaylistMenu";
+import { Dispatch } from "../../types";
 
-const SelectionMenu = props => (
+interface DispatchProps {
+  invertSelection: () => void;
+  selectZero: () => void;
+  selectAll: () => void;
+}
+
+const SelectionMenu = (props: DispatchProps) => (
   <PlaylistMenu id="playlist-selection-menu">
     <div className="invert-selection" onClick={props.invertSelection} />
     <div className="select-zero" onClick={props.selectZero} />
@@ -11,10 +18,12 @@ const SelectionMenu = props => (
   </PlaylistMenu>
 );
 
-const mapDispatchToProps = {
-  invertSelection: () => ({ type: INVERT_SELECTION }),
-  selectAll: () => ({ type: SELECT_ALL }),
-  selectZero: () => ({ type: SELECT_ZERO })
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
+  return {
+    invertSelection: () => dispatch({ type: INVERT_SELECTION }),
+    selectAll: () => dispatch({ type: SELECT_ALL }),
+    selectZero: () => dispatch({ type: SELECT_ZERO })
+  };
 };
 export default connect(
   null,
