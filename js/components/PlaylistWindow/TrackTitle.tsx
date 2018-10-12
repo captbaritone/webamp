2a0ea51bca4d03e2b032d579f788578df5ac3fe2
Jordan Eldredge <jordan@jordaneldredge.com>
2018-10-11 21:31:10 -0700

    Type Track title

--- js/components/PlaylistWindow/TrackTitle.tsx
+++ js/components/PlaylistWindow/TrackTitle.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getTrackDisplayName } from "../../selectors";
+import { AppState } from "../../types";
 
-const TrackTitle = props => (
+interface OwnProps {
+  id: number;
+  paddedTrackNumber: string;
+}
+
+interface StateProps {
+  title: string | null;
+}
+
+const TrackTitle = (props: OwnProps & StateProps) => (
   <span>
     {props.paddedTrackNumber}. {props.title}
   </span>
 );
 
-const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = (state: AppState, ownProps: OwnProps): StateProps => ({
   title: getTrackDisplayName(state)(ownProps.id)
 });
 
