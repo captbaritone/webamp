a52c63ea427b895e7267dc8207a5f31b43e7b9ee
Jordan Eldredge <jordan@jordaneldredge.com>
2018-10-11 20:33:46 -0700

    Type ScrollBar

--- js/components/PlaylistWindow/ScrollBar.tsx
+++ js/components/PlaylistWindow/ScrollBar.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
-import Slider from "rc-slider/lib/Slider";
+import Slider from "rc-slider";
 
 import { setPlaylistScrollPosition } from "../../actionCreators";
 import { getVisibleTrackIds, getPlaylistScrollPosition } from "../../selectors";
+import { AppState, Dispatch } from "../../types";
+
+interface StateProps {
+  playlistScrollPosition: number;
+  allTracksAreVisible: boolean;
+}
+
+interface DispatchProps {
+  setPlaylistScrollPosition: (value: number) => void;
+}
 
 const Handle = () => <div className="playlist-scrollbar-handle" />;
 
-const ScrollBar = props => (
+const ScrollBar = (props: StateProps & DispatchProps) => (
   <Slider
     className="playlist-scrollbar"
-    type="range"
     min={0}
     max={100}
     step={1}
@@ -22,15 +31,17 @@ const ScrollBar = props => (
   />
 );
 
-const mapDispatchToProps = {
-  setPlaylistScrollPosition: position =>
-    setPlaylistScrollPosition(100 - position)
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
+  return {
+    setPlaylistScrollPosition: position =>
+      dispatch(setPlaylistScrollPosition(100 - position))
+  };
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: AppState): StateProps => ({
   playlistScrollPosition: getPlaylistScrollPosition(state),
   allTracksAreVisible:
-    getVisibleTrackIds(state).length === state.playlist.length
+    getVisibleTrackIds(state).length === state.playlist.trackOrder.length
 });
 
 export default connect(
