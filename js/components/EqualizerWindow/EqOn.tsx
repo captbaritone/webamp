c9340f7884399c51b296e25941506999560175bb
Jordan Eldredge <jordan@jordaneldredge.com>
2018-10-13 13:49:05 -0700

    Type EqOn

---
+++ js/components/EqualizerWindow/EqOn.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { connect, DispatchProp } from "react-redux";
+import classnames from "classnames";
+
+import { toggleEq } from "../../actionCreators";
+import { AppState, Dispatch } from "../../types";
+
+interface StateProps {
+  on: boolean;
+}
+
+interface DispatchProps {
+  toggleEq(): void;
+}
+
+const EqOn = (props: StateProps & DispatchProps) => {
+  return (
+    <div
+      id="on"
+      className={classnames({
+        selected: props.on
+      })}
+      onClick={props.toggleEq}
+    />
+  );
+};
+
+const mapStateToProps = (state: AppState): StateProps => ({
+  on: state.equalizer.on
+});
+
+const mapDispatchProps = (dispatch: Dispatch): DispatchProps => {
+  return { toggleEq: () => dispatch(toggleEq()) };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchProps
+)(EqOn);
