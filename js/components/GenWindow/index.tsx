9e3fbb699d1ccf5c530c3c31b069c7dfb0740d60
Jordan Eldredge <jordan@jordaneldredge.com>
2018-10-15 18:53:02 -0700

    Remoe unused import

--- js/components/GenWindow/index.tsx
+++ js/components/GenWindow/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import PropTypes from "prop-types";
 import classnames from "classnames";
 import "../../../css/gen-window.css";
 
