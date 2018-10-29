9635672652f40372f5ca00f7c3b525721d6ff4d9
Jordan Eldredge <jordan@jordaneldredge.com>
2018-10-28 21:21:57 -0700

    Remove useless class

--- js/components/CharacterString.tsx
+++ js/components/CharacterString.tsx
@@ -5,18 +5,12 @@ interface Props {
   children: string;
 }
 
-class CharacterString extends React.Component<Props> {
-  shouldComponentUpdate(nextProps: Props) {
-    return nextProps.children !== this.props.children;
-  }
-
-  render() {
-    const text = `${this.props.children}` || "";
-    const chars = text.split("");
-    return chars.map((character, index) => (
-      <Character key={index + character}>{character}</Character>
-    ));
-  }
-}
+const CharacterString = (props: Props) => {
+  const text = `${props.children}` || "";
+  const chars = text.split("");
+  return chars.map((character, index) => (
+    <Character key={index + character}>{character}</Character>
+  ));
+};
 
 export default CharacterString;
