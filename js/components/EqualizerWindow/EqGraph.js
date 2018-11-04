0bc45f6cfec0dc9c1a7817527f9e75f3c211b7c4
Jordan Eldredge <jordan@jordaneldredge.com>
2018-11-03 21:57:31 -0700

    Fix import order

--- js/components/EqualizerWindow/EqGraph.js
+++ js/components/EqualizerWindow/EqGraph.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
+import { percentToRange, clamp } from "../../utils";
+import { BANDS } from "../../constants";
 import { getCurvePoints } from "./spline";
 import line from "./bresenham";
-import { percentToRange, clamp } from "../../utils";
-
-import { BANDS } from "../../constants";
 
 const GRAPH_HEIGHT = 19;
 const GRAPH_WIDTH = 113;
