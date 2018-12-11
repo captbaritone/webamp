8d914b8eb5094afb973934d51ba9a801ca127677
Jordan Eldredge <jordan@jordaneldredge.com>
2018-12-10 17:36:14 -0800

    Unregister service workers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { createStore } from "./redux/store";
 import App from "./App";
 // import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 
 ReactDOM.render(
   <Provider store={createStore()}>
@@ -12,3 +13,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 // registerServiceWorker();
+
+// At one point I had serivce workers installed. Then I removed it.
+// It seems to have lead to apps never getting updated.
+unregister();
