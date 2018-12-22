c8e77ea5f58932962a0a947fd2ba11eada12a9dc
Jordan Eldredge <jordan@jordaneldredge.com>
2018-12-22 10:29:17 -0800

    Use reselect

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,4 @@
+import { createSelector } from "reselect";
 import * as Utils from "../utils";
 import { ABOUT_PAGE } from "../constants";
 
@@ -13,25 +14,39 @@ export function overlayShouldAnimate(state) {
   return getSelectedSkinPosition(state) != null;
 }
 
-export function getSelectedSkinUrl(state) {
-  const hash = getSelectedSkinHash(state);
-  return hash == null ? null : Utils.screenshotUrlFromHash(hash);
-}
+export const getSelectedSkinUrl = createSelector(
+  getSelectedSkinHash,
+  hash => {
+    return hash == null ? null : Utils.screenshotUrlFromHash(hash);
+  }
+);
 
 export function getSearchQuery(state) {
   return state.searchQuery;
 }
 
-// TODO: Memoize this very expensive function
-export function getMatchingSkinHashes(state) {
-  const skinHashes = getSkinHashes(state);
-  const searchQuery = getSearchQuery(state);
-  if (searchQuery == null || state.matchingHashes == null) {
-    return skinHashes;
-  }
-  return skinHashes.filter(hash => state.matchingHashes.has(hash));
+export function getMatchingHashes(state) {
+  return state.matchingHashes;
 }
 
+export const getSkinHashes = createSelector(
+  getSkins,
+  skins => Object.keys(skins)
+);
+
+export const getMatchingSkinHashes = createSelector(
+  getSkinHashes,
+  getSearchQuery,
+  getMatchingHashes,
+  (skinHashes, searchQuery, matchingHashes) => {
+    if (searchQuery == null || matchingHashes == null) {
+      return skinHashes;
+    }
+    return skinHashes.filter(hash => matchingHashes.has(hash));
+  }
+);
+
+// We should be careful _not_ to memoize this function since it's non determinisitc
 export function getRandomSkinHash(state) {
   const skinHashes = getSkinHashes(state);
   const numberOfSkins = skinHashes.length;
@@ -39,37 +54,37 @@ export function getRandomSkinHash(state) {
   return skinHashes[randomIndex];
 }
 
-export function getUrl(state) {
-  if (state.activeContentPage === ABOUT_PAGE) {
-    return "/about/";
+export const getUrl = createSelector(
+  getActiveContentPage,
+  getSelectedSkinHash,
+  getSearchQuery,
+  (activeContentPage, hash, query) => {
+    if (activeContentPage === ABOUT_PAGE) {
+      return "/about/";
+    }
+    if (hash) {
+      // TODO: Add a human readable version
+      return Utils.getPermalinkUrlFromHash(hash);
+    } else if (query) {
+      return `/?query=${encodeURIComponent(query)}`;
+    }
+    return "/";
   }
-  const hash = getSelectedSkinHash(state);
-  const query = getSearchQuery(state);
-  if (hash) {
-    // TODO: Add a human readable version
-    return Utils.getPermalinkUrlFromHash(hash);
-  } else if (query) {
-    return `/?query=${encodeURIComponent(query)}`;
-  }
-  return "/";
-}
+);
 
 export function getPageTitle(state) {
   return "Winamp Skin Museum";
 }
 
-export function getPreviewImageUrl(state) {
-  const hash = getSelectedSkinHash(state);
-  return hash == null ? null : Utils.screenshotUrlFromHash(hash);
-}
+export const getPreviewImageUrl = createSelector(
+  getSelectedSkinHash,
+  hash => {
+    return hash == null ? null : Utils.screenshotUrlFromHash(hash);
+  }
+);
 
 export function getActiveContentPage(state) {
   return state.activeContentPage;
-}
-
-// TODO: This could be memoized
-export function getSkinHashes(state) {
-  return Object.keys(getSkins(state));
 }
 
 export function getSkins(state) {
