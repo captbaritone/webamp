df93ec482cb176ccb62d37d5e5a9a922f5b8bedd
Jordan Eldredge <jordan@jordaneldredge.com>
2018-12-30 15:56:29 -0800

    Type Shade button

--- js/components/MainWindow/Shade.tsx
+++ js/components/MainWindow/Shade.tsx
@@ -3,8 +3,13 @@ import { connect } from "react-redux";
 import ClickedDiv from "../ClickedDiv";
 
 import { toggleMainWindowShadeMode } from "../../actionCreators";
+import { Dispatch } from "../../types";
 
-const Shade = props => (
+interface DispatchProps {
+  handleClick(): void;
+}
+
+const Shade = (props: DispatchProps) => (
   <ClickedDiv
     id="shade"
     onMouseDown={props.handleClick}
@@ -13,11 +18,13 @@ const Shade = props => (
   />
 );
 
-const mapDispatchToProps = {
-  handleClick: toggleMainWindowShadeMode
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
+  return {
+    handleClick: () => dispatch(toggleMainWindowShadeMode())
+  };
 };
 
 export default connect(
-  () => ({}),
+  null,
   mapDispatchToProps
 )(Shade);
