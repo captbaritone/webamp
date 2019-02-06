98ca95749cb19f410f85feb929876ece18017724
Jordan Eldredge <jordan@jordaneldredge.com>
2019-02-06 17:40:10 -0500

    Add local dir

--- js/Disposable.ts
+++ js/Disposable.ts
@@ -2,12 +2,27 @@ type Teardown = (() => void) | { dispose: () => void };
 
 export default class Disposable {
   _teardowns: Teardown[] = [];
+  disposed: boolean;
+
+  constructor() {
+    this.disposed = false;
+  }
 
   add(...teardowns: Teardown[]): void {
+    if (this.disposed) {
+      throw new Error(
+        "Attempted to add a new teardown to a disposed disposable."
+      );
+    }
     this._teardowns.push(...teardowns);
   }
 
   dispose() {
+    if (this.disposed) {
+      throw new Error(
+        "Attempted to dispose disposable which is already disposed."
+      );
+    }
     this._teardowns.forEach(teardown => {
       if (typeof teardown === "function") {
         teardown();
@@ -15,5 +30,6 @@ export default class Disposable {
         teardown.dispose();
       }
     });
+    this.disposed = true;
   }
 }
