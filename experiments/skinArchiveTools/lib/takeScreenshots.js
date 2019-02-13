1292bb08251b91478af7df982a66ef4d631f5d5e
Jordan Eldredge <jordan@jordaneldredge.com>
2019-02-13 11:38:44 -0800

    Speed up screenshots

--- experiments/skinArchiveTools/lib/takeScreenshots.js
+++ experiments/skinArchiveTools/lib/takeScreenshots.js
@@ -1,26 +1,36 @@
 const path = require("path");
 const fs = require("fs");
-const md5File = require("md5-file/promise");
-const Filehound = require("filehound");
+const fsPromises = require("fs").promises;
 const Shooter = require("./shooter");
 
 module.exports = async function takeScreenshots(skinDir, screenshotDir) {
-  const files = await Filehound.create()
-    .ext(["wsz"])
-    .paths(skinDir)
-    .find();
+  console.log(`Looking for skins in ${skinDir}`);
+  const files = await fsPromises.readdir(skinDir);
+  console.log(`Found ${files.length} skins`);
+  let skipCount = 0;
+  let shotCount = 0;
+  const interval = setInterval(() => {
+    console.log(`Update: Skipped: ${skipCount}`);
+    console.log(`Update: Shot: ${shotCount}`);
+  }, 10000);
   const shooter = new Shooter();
-  for (const filePath of files) {
+  for (const fileName of files) {
+    const filePath = path.join(skinDir, fileName);
     if (!fs.existsSync(filePath)) {
       throw new Error("wat", filePath);
     }
-    const md5 = await md5File(filePath);
+    const md5 = path.basename(filePath, ".wsz");
     const screenshotPath = path.join(screenshotDir, `${md5}.png`);
     if (fs.existsSync(screenshotPath)) {
+      skipCount++;
       continue;
     }
     await shooter.takeScreenshot(filePath, screenshotPath, {
       minify: true
     });
+    shotCount++;
   }
+  clearInterval(interval);
+  console.log(`FINAL: Skipped: ${skipCount}`);
+  console.log(`FINAL: Shot: ${shotCount}`);
 };
