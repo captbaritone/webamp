c5011ca3d29c6af95ad94a0f3fb6871c4005ddc7
Jordan Eldredge <jordan@jordaneldredge.com>
2019-03-03 15:00:57 -0800

    Type Eject.js

---
+++ js/components/MainWindow/Eject.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import * as Actions from "../../actionCreators";
+import { Dispatch } from "../../types";
+
+interface DispatchProps {
+  openMediaFileDialog(): void;
+}
+
+const Eject = (props: DispatchProps) => (
+  <div id="eject" onClick={props.openMediaFileDialog} title="Open File(s)" />
+);
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  openMediaFileDialog: () => dispatch(Actions.openMediaFileDialog())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Eject);
