d5e55aa1f76f3ea7f93ff1efbf31c7971fc1370d
Jordan Eldredge <jordan@jordaneldredge.com>
2019-03-03 22:10:40 -0800

    Remove flaky test

--- scripts/postcss-optimize-data-uri-pngs.test.js
+++ scripts/postcss-optimize-data-uri-pngs.test.js
@@ -4,7 +4,8 @@ const postcss = require("postcss");
 const plugin = require("./postcss-optimize-data-uri-pngs");
 
 const css = fs.readFileSync("./css/base-skin.css", "utf8");
-it("does something", async () => {
+// This seems to timeout a lot on Travis
+it.skip("does something", async () => {
   const result = await postcss([plugin({})]).process(css);
   expect(result.css.length).toBeLessThan(css.length);
 });
