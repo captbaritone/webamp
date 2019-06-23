d820b8c830f2a9a66434bb3abc4bef66d8de92dc
Jordan Eldredge <jordan@jordaneldredge.com>
2019-06-23 17:21:36 -0400

    Start playing with a Winston client that DMs me

--- experiments/skin-database/logger.js
+++ experiments/skin-database/logger.js
@@ -1,18 +1,16 @@
-const { createLogger, format, transports } = require('winston');
+const { createLogger, format, transports } = require("winston");
 
 const logger = createLogger({
-  level: 'info',
+  level: "info",
   format: format.combine(
     format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
+      format: "YYYY-MM-DD HH:mm:ss",
     }),
     format.errors({ stack: true }),
     format.splat(),
     format.json()
   ),
-  transports: [
-    new transports.File({ filename: 'combined.log' })
-  ]
+  transports: [new transports.File({ filename: "combined.log" })],
 });
 
 module.exports = logger;
