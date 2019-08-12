fc2a015ae1a195910a6b19bf71e331b37e4fbe0b
jberg <jberg@users.noreply.github.com>
2019-08-12 16:33:38 -0700

    Fix event x/y by finding container node (#849)
    * Fix event x/y by finding relativeParent
    
    * use a tree search to find the container instead of event target hacks

--- modern/src/runtime/GuiObject.js
+++ modern/src/runtime/GuiObject.js
@@ -56,19 +56,19 @@ class GuiObject extends MakiObject {
   }
 
   gettop() {
-    return this.attributes.y || 0;
+    return Number(this.attributes.y) || 0;
   }
 
   getleft() {
-    return this.attributes.x || 0;
+    return Number(this.attributes.x) || 0;
   }
 
   getheight() {
-    return this.attributes.h || 0;
+    return Number(this.attributes.h) || 0;
   }
 
   getwidth() {
-    return this.attributes.w || 0;
+    return Number(this.attributes.w) || 0;
   }
 
   resize(x, y, w, h) {
