9ed01e9b89ab90fcaf8c76dd112d4928686777c8
Jordan Eldredge <jordan@jordaneldredge.com>
2019-08-14 21:30:27 -0700

    Type Emitter

--- modern/src/Emitter.ts
+++ modern/src/Emitter.ts
@@ -1,11 +1,14 @@
+// TODO: Merge with the Webamp emitter
 class Emitter {
+  _hooks: { [eventName: string]: Array<(...args: any[]) => void> };
+  _globalHooks: Array<(eventName: string, ...args: any[]) => void>;
   constructor() {
     this._hooks = {};
     // TODO: Rename this property
     this._globalHooks = [];
   }
 
-  listen(eventName, cb) {
+  listen(eventName: string, cb: (...args: any[]) => void) {
     if (this._hooks[eventName] == null) {
       this._hooks[eventName] = [];
     }
@@ -17,7 +20,7 @@ class Emitter {
     };
   }
 
-  trigger(eventName, ...args) {
+  trigger(eventName: string, ...args: any[]) {
     this._globalHooks.map(cb => cb(eventName, ...args));
     if (this._hooks[eventName] == null) {
       return;
@@ -25,7 +28,7 @@ class Emitter {
     this._hooks[eventName].map(cb => cb(...args));
   }
 
-  listenToAll(cb) {
+  listenToAll(cb: (eventName: string, ...args: any[]) => void) {
     this._globalHooks.push(cb);
     return () => {
       this._globalHooks = this._globalHooks.filter(hookCb => hookCb !== cb);
