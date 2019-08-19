bb5e359197397928053dbd0ac8a0f146169f00b7
jberg <jordannealberg@gmail.com>
2019-08-19 09:57:47 -0700

    Add missing config classes

---
+++ modern/src/runtime/ConfigItem.js
@@ -0,0 +1,15 @@
+import MakiObject from "./MakiObject";
+
+class ConfigItem extends MakiObject {
+  /**
+   * getclassname()
+   *
+   * Returns the class name for the object.
+   * @ret The class name.
+   */
+  getclassname() {
+    return "ConfigItem";
+  }
+}
+
+export default ConfigItem;
