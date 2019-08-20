e3b3eab63e84fe97794f96b20a8567d8b37e6591
Jordan Eldredge <jordan@jordaneldredge.com>
2019-08-20 07:42:54 -0700

    Make AlbumsTable a functional component

--- js/components/MediaLibraryWindow/AlbumsTable.tsx
+++ js/components/MediaLibraryWindow/AlbumsTable.tsx
@@ -1,16 +1,14 @@
 import * as React from "react";
 import LibraryTable from "./LibraryTable";
 
-interface Props {}
+const AlbumsTable = React.memo(() => {
+  return (
+    <LibraryTable
+      headings={["Album", "Tracks"]}
+      rows={[["All (1 album)", "1"], ["Ben Mason", "1"]]}
+      widths={[50, 200]}
+    />
+  );
+});
 
-export default class AlbumsTable extends React.Component<Props> {
-  render() {
-    return (
-      <LibraryTable
-        headings={["Album", "Tracks"]}
-        rows={[["All (1 album)", "1"], ["Ben Mason", "1"]]}
-        widths={[50, 200]}
-      />
-    );
-  }
-}
+export default AlbumsTable;
