d17333d4284ff1702cd662fa08fba5f2266652d5
jberg <jberg@users.noreply.github.com>
2019-08-21 21:04:13 -0700

    Add missing config classes (#874)
    * Add missing config classes
    
    * fix test

---
+++ modern/src/runtime/ConfigAttribute.js
@@ -0,0 +1,15 @@
+import MakiObject from "./MakiObject";
+
+class ConfigAttribute extends MakiObject {
+  /**
+   * getclassname()
+   *
+   * Returns the class name for the object.
+   * @ret The class name.
+   */
+  getclassname() {
+    return "ConfigAttribute";
+  }
+}
+
+export default ConfigAttribute;
