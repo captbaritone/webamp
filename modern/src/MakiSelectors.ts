729d651a604b89ad51258e329f572639617ad3c9
Jordan Eldredge <jordan@jordaneldredge.com>
2019-08-24 21:37:39 -0700

    Add some generic methods for dealing with selectors

--- modern/src/MakiSelectors.ts
+++ modern/src/MakiSelectors.ts
@@ -1,14 +1,18 @@
 import * as Utils from "./utils";
 
-function findNodeByUid(state, uid) {
-  return Utils.findInTree(state.xmlTree, node => {
-    return node.uid === uid;
-  });
+function findNodeByUid(uid) {
+  // TODO: Do some clever caching here.
+  return state => {
+    return Utils.findInTree(state.xmlTree, node => {
+      return node.uid === uid;
+    });
+  };
 }
 
 export function getTop(uid) {
+  const findNodeInState = findNodeByUid(uid);
   return state => {
-    const node = findNodeByUid(state, uid);
+    const node = findNodeInState(state);
     return Number(node.attributes.y) || 0;
   };
 }
