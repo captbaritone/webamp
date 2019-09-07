a18d5ba0ee6de4aacdcb299e34414132aaaafc89
jberg <jordannealberg@gmail.com>
2019-09-07 12:18:51 -0700

    Add getVUMeter functions

--- modern/src/Selectors.ts
+++ modern/src/Selectors.ts
@@ -7,3 +7,11 @@ export function getMakiTree(state: ModernAppState): MakiTree | null {
 export function getVolume(state: ModernAppState): number {
   return state.volume;
 }
+
+export function getRightVUMeter(state: ModernAppState): number {
+  return state.rightVUMeter;
+}
+
+export function getLeftVUMeter(state: ModernAppState): number {
+  return state.leftVUMeter;
+}
