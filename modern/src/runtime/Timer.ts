3743868cc6a766a45cc52ccac3fd6f4c86b44df4
jberg <jberg@users.noreply.github.com>
2019-09-10 09:17:26 -0700

    Implement timer (#909)
    * Implement timer
    
    * change isRunning in objects to match Winamp behavior, std.mi is wrong
    
    * need to set start time for timer
    
    * add comment to updated objects array

--- modern/src/runtime/Timer.ts
+++ modern/src/runtime/Timer.ts
@@ -2,6 +2,28 @@ import MakiObject from "./MakiObject";
 import { unimplementedWarning } from "../utils";
 
 class Timer extends MakiObject {
+  _speed: number;
+  _animationStartTime: number;
+  _animationCancelID: number;
+
+  constructor(node, parent, annotations, store) {
+    super(node, parent, annotations, store);
+
+    this._speed = 200;
+    this._animationStartTime = 0;
+    this._animationCancelID = null;
+  }
+
+  _animationLoop(): void {
+    this._animationCancelID = window.requestAnimationFrame(currentTime => {
+      if (currentTime > this._animationStartTime + this._speed) {
+        this._animationStartTime = currentTime;
+        this.ontimer();
+      }
+      this._animationLoop();
+    });
+  }
+
   /**
    * getclassname()
    *
@@ -12,31 +34,30 @@ class Timer extends MakiObject {
     return "Timer";
   }
 
-  setdelay(millisec: number) {
-    unimplementedWarning("setDelay");
+  setdelay(millisec: number): void {
+    this._speed = millisec;
   }
 
-  start() {
-    unimplementedWarning("start");
+  start(): void {
+    this._animationStartTime = performance.now();
+    this._animationLoop();
   }
 
-  stop() {
-    unimplementedWarning("stop");
+  stop(): void {
+    window.cancelAnimationFrame(this._animationCancelID);
+    this._animationCancelID = null;
   }
 
-  ontimer() {
-    unimplementedWarning("ontimer");
-    return;
+  ontimer(): void {
+    this.js_trigger("onTimer");
   }
 
-  getdelay() {
-    unimplementedWarning("getdelay");
-    return;
+  getdelay(): number {
+    return this._speed;
   }
 
-  isrunning() {
-    unimplementedWarning("isrunning");
-    return;
+  isrunning(): boolean {
+    return this._animationCancelID != null;
   }
 
   getskipped() {
