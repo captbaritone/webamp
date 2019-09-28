2e21057b9431571ced01b761c9294120f80027f1
Jordan Eldredge <jordan@jordaneldredge.com>
2019-09-28 14:51:34 -0700

    Pull object definitions out into json files

---
+++ modern/src/maki-interpreter/objectData/stdPatched.js
@@ -0,0 +1,33 @@
+import std from "./std.json";
+
+// Between myself and the author of the decompiler, a number of manual tweaks
+// have been made to our current object definitions. This function recreates
+// those tweaks so we can have an apples to apples comparison.
+
+/*
+ * From object.js
+ *
+ * > The std.mi has this set as void, but we checked in Winamp and confirmed it
+ * > returns 0/1
+ */
+std["5D0C5BB67DE14b1fA70F8D1659941941"].functions[5].result = "boolean";
+
+/*
+ * From Object.pm
+ *
+ * > note, std.mi does not have this parameter!
+ */
+std.B4DCCFFF81FE4bcc961B720FD5BE0FFF.functions[0].parameters[0][1] = "onoff";
+
+/*
+ * From Object.pm
+ *
+ * > note, my std.mi did not contain this!
+ */
+std.B4DCCFFF81FE4bcc961B720FD5BE0FFF.functions.push({
+  parameters: [],
+  name: "getCurCfgVal",
+  result: "Int",
+});
+
+export default std;
