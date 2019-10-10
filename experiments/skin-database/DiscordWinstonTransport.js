9ca10abdb45170bb16c1218b514be44f98b2cf8d
Jordan Eldredge <jordan@jordaneldredge.com>
2019-10-10 09:32:12 -0400

    Improve Discord logging

--- experiments/skin-database/DiscordWinstonTransport.js
+++ experiments/skin-database/DiscordWinstonTransport.js
@@ -7,8 +7,17 @@ class DiscordWinstonTransport extends Transport {
   }
 
   async log(info, callback) {
-    const { message } = info;
-    await this._channel.send(`${message}`);
+    const { message, ...rest } = info;
+    let dataString = null;
+    try {
+      dataString = JSON.stringify(rest, null, 2);
+    } catch (e) {
+      dataString = "COULD NOT STRINGIFY DATA";
+    }
+    await this._channel.send(`${message}
+\`\`\`
+${dataString}
+\`\`\``);
     // Perform the writing to the remote service
     callback();
   }
