a8df0dd575a81f2a0e2c4e9163503456b0c9cd32
Jordan Eldredge <jordan@jordaneldredge.com>
2019-10-14 08:52:25 -0700

    Make typing of MakiObjects more explicit

--- modern/src/utils.ts
+++ modern/src/utils.ts
@@ -287,9 +287,7 @@ function findDirectDescendantById<
   );
 }
 
-function* iterateLexicalScope<
-  T extends { children: T[]; parent: T | undefined }
->(node: T): IterableIterator<T> {
+function* iterateLexicalScope(node: MakiObject): IterableIterator<MakiObject> {
   let currentNode = node;
   while (currentNode.parent) {
     const { parent } = currentNode;
@@ -306,10 +304,10 @@ function* iterateLexicalScope<
 }
 
 // Search up the tree for a node that is in `node`'s lexical that matches `predicate`.
-function findInLexicalScope<T extends { children: T[]; parent: T | undefined }>(
-  node: T,
-  predicate: (node: T) => boolean
-): T | null {
+function findInLexicalScope(
+  node: MakiObject,
+  predicate: (node: MakiObject) => boolean
+): MakiObject | null {
   for (const child of iterateLexicalScope(node)) {
     if (predicate(child)) {
       return child;
