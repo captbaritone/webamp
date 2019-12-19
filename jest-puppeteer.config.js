4518c49371b4edefe3c1eea9212ef28b2bc3128e
Jordan Eldredge <jordan@jordaneldredge.com>
2019-12-19 06:42:14 -0800

    Fix typo

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   server: {
     // Note: We require the the build be run before these tests.
-    // TODO: Figure out how to get this command to run the buil.
+    // TODO: Figure out how to get this command to run the build.
     command: "npm run serve",
     port: 8080, // Jest Puppeteer will wait 5000ms for this port to come online.
   },
