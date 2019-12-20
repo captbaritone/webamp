080bf26e5383251e63f0ee44c967e40c3be00ee8
Jordan Eldredge <jordan@jordaneldredge.com>
2019-12-19 20:58:10 -0800

    Format

--- examples/webpack/index.js
+++ examples/webpack/index.js
@@ -5,11 +5,11 @@ new Webamp({
     {
       metaData: {
         artist: "DJ Mike Llama",
-        title: "Llama Whippin' Intro",
+        title: "Llama Whippin' Intro"
       },
       url:
         "https://cdn.jsdelivr.net/gh/captbaritone/webamp@43434d82cfe0e37286dbbe0666072dc3190a83bc/mp3/llama-2.91.mp3",
-      duration: 5.322286,
-    },
-  ],
+      duration: 5.322286
+    }
+  ]
 }).renderWhenReady(document.getElementById("app"));
