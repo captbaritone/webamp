a3d6615f3ff70efd1e572c17ea05e75cb572660c
Jordan Eldredge <jordan@jordaneldredge.com>
2019-12-22 15:53:30 -0800

    Split up Maki eslint rules

---
+++ modern/eslint/maki-eslint-utils.js
@@ -0,0 +1,26 @@
+const { objects } = require("../src/maki-interpreter/objects");
+
+const classNameMappings = {
+  Object: "MakiObject",
+  "@{00000000-0000-0000-0000-000000000000}@": null,
+};
+
+function normalizeClassName(className) {
+  const normalized = classNameMappings[className];
+  return normalized === undefined ? className : normalized;
+}
+
+const objectsByName = {};
+for (const value of Object.values(objects)) {
+  objectsByName[normalizeClassName(value.name)] = value;
+}
+
+function getMakiObjectfromClassDeclarationNode(node) {
+  const className = node.id.name;
+  return objectsByName[className];
+}
+
+module.exports = {
+  getMakiObjectfromClassDeclarationNode,
+  normalizeClassName,
+};
