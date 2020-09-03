5bdab52b05f7841ab1eef846776e28a8b451b4f4
Jordan Eldredge <jordan@jordaneldredge.com>
2020-09-03 07:26:44 -0700

    About page cleanup

--- src/About.js
+++ src/About.js
@@ -7,29 +7,29 @@ function About() {
       <p>
         The Winamp Skin Museum is an attempt to build a <i>fast</i>,{" "}
         <i>searchable</i>, and <i>shareable</i>, interface for the collection of
-        Winamp Skins amassed by the{" "}
+        Winamp Skins amassed on the{" "}
         <a href="https://archive.org/details/winampskins">Internet Archive</a>.
       </p>
       <h2>Features:</h2>
       <ul>
         <li>
-          <strong>Infinite scroll</strong> preview images.
+          <strong>Infinite scroll</strong> preview images
         </li>
         <li>
           <strong>Experience</strong> skins with integrated{" "}
-          <a href="https://webamp.org">Webamp</a>.
+          <a href="https://webamp.org">Webamp</a>
         </li>
         <li>
-          <strong>Fast search</strong> of indexed readme.txt texts.
+          <strong>Fast search</strong> of indexed readme.txt texts
         </li>
       </ul>
       <p>
         Made by <a href="https://jordaneldredge.com">Jordan Eldredge</a> (
-        <a href="https://twitter.com/captbaritone">@captbaritone</a>).
+        <a href="https://twitter.com/captbaritone">@captbaritone</a>)
       </p>
       <p>
-        Come hang out with us in our{" "}
-        <a href="https://discord.gg/fBTDMqR">Discord server</a>.
+        You might enjoy my Twitter bot{" "}
+        <a href="https://twitter.com/winampskins">@winampskins</a>
       </p>
     </div>
   );
