5b7057e1d4c6f0849cde141de17704de5387bc3f
Jordan Eldredge <jordan@jordaneldredge.com>
2020-11-12 11:25:31 -0800

    Use last matching file
    See comment in the code.
    
    Here is the skin that originally surfaced this issue: https://skins.webamp.org/skin/a0cf0f893d345eba99809fc84af8a71c/Half-Life%202%20-%20Gordon.wsz/

--- packages/webamp/js/skinParserUtils.ts
+++ packages/webamp/js/skinParserUtils.ts
@@ -24,20 +24,30 @@ export async function getFileFromZip(
     return null;
   }
 
-  let contents;
+  // Windows file system is case insensitve, but zip files are not.
+  // This means that it's possible for a zip to contain both `main.bmp` _and_
+  // `main.BMP` but in Winamp only one will be materialized onto disk when
+  // decompressing. I suspect that in this case later files in the archive
+  // overwrite earlier ones. To mimic that behavior we use the last matching
+  // file.
+  //
+  // This works because `JSZip.file` filters the files by iterating the
+  // underlying `files` object under the hood:
+  // https://github.com/Stuk/jszip/blob/25d401e104926fef8528d670ecfe53f14e77a297/lib/object.js#L182
+  // Since JavaScript objects are iterable in the order that the keys were
+  // added this _should_ mean that by taking the last file here we will get
+  // the last file that JSZip extracted.
+  const lastFile = files[files.length - 1];
+
   try {
-    contents = await files[0].async(mode);
+    const contents = await lastFile.async(mode);
+    return { contents, name: lastFile.name };
   } catch (e) {
     console.warn(
       `Failed to extract "${fileName}.${ext}" from the skin archive.`
     );
     return null;
   }
-  // Return a promise (awaitable).
-  return {
-    contents,
-    name: files[0].name,
-  };
 }
 
 function fallbackGetImgFromBlob(blob: Blob): Promise<HTMLImageElement> {
