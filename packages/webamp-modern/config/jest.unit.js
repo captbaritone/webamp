450180a1e364d28e7e89350e5956e46be996ac0a
Jordan Eldredge <jordan@jordaneldredge.com>
2021-05-30 14:34:43 -0700

    Remove unused mocks

--- packages/webamp-modern/config/jest.unit.js
+++ packages/webamp-modern/config/jest.unit.js
@@ -7,9 +7,9 @@ module.exports = {
   },
   moduleFileExtensions: ["js", "tsx", "ts"],
   moduleNameMapper: {
-    "\\.css$": "<rootDir>/js/__mocks__/styleMock.js",
-    "\\.wsz$": "<rootDir>/js/__mocks__/fileMock.js",
-    "\\.mp3$": "<rootDir>/js/__mocks__/fileMock.js",
+    // "\\.css$": "<rootDir>/js/__mocks__/styleMock.js",
+    // "\\.wsz$": "<rootDir>/js/__mocks__/fileMock.js",
+    // "\\.mp3$": "<rootDir>/js/__mocks__/fileMock.js",
   },
   transform: {
     "^.+\\.(js|ts|tsx)$": "babel-jest",
