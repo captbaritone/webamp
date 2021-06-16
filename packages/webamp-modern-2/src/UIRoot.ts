e82f3ccdb380b6e5f47ffc007cc06743dbad288f
Jordan Eldredge <jordan@jordaneldredge.com>
2021-06-16 13:27:08 -0700

    More font properties, including True Type fonts

--- packages/webamp-modern-2/src/UIRoot.ts
+++ packages/webamp-modern-2/src/UIRoot.ts
@@ -1,9 +1,11 @@
 import Bitmap from "./skin/Bitmap";
+import TrueTypeFont from "./skin/TrueTypeFont";
 import { assert } from "./utils";
 
 class UIRoot {
   // Just a temporary place to stash things
   _bitmaps: Bitmap[] = [];
+  _trueTypeFonts: TrueTypeFont[] = [];
 
   addBitmap(bitmap: Bitmap) {
     this._bitmaps.push(bitmap);
@@ -12,6 +14,19 @@ class UIRoot {
   getBitmap(id: string): Bitmap {
     const found = this._bitmaps.find(
       (bitmap) => bitmap._id.toLowerCase() === id.toLowerCase()
+    );
+
+    assert(found != null, `Could not find bitmap with id ${id}.`);
+    return found;
+  }
+
+  addTrueTypeFont(font: TrueTypeFont) {
+    this._trueTypeFonts.push(font);
+  }
+
+  getTrueTypeFont(id: string): TrueTypeFont {
+    const found = this._trueTypeFonts.find(
+      (font) => font._id.toLowerCase() === id.toLowerCase()
     );
 
     assert(found != null, `Could not find bitmap with id ${id}.`);
--- packages/webamp-modern-2/src/skin/Button.ts
+++ packages/webamp-modern-2/src/skin/Button.ts
@@ -1,16 +1,39 @@
 import GuiObj from "./GuiObj";
+import UI_ROOT from "../UIRoot";
+import { px } from "../utils";
 
 // http://wiki.winamp.com/wiki/XML_GUI_Objects#.3Cbutton.2F.3E_.26_.3Ctogglebutton.2F.3E
 export default class Button extends GuiObj {
+  _image: string;
   setXmlAttr(key: string, value: string): boolean {
     if (super.setXmlAttr(key, value)) {
       return true;
     }
     switch (key) {
+      case "image":
+        this._image = value;
+        break;
       default:
         return false;
     }
     return true;
+  }
+
+  getDebugDom(): HTMLDivElement {
+    const div = super.getDebugDom();
+    div.setAttribute("data-obj-name", "Button");
+    if (this._image != null) {
+      const bitmap = UI_ROOT.getBitmap(this._image);
+      div.style.backgroundImage = bitmap.getBackgrondImageCSSAttribute();
+      div.style.backgroundPosition = bitmap.getBackgrondPositionCSSAttribute();
+      if (div.style.width === "" && bitmap.getWidth()) {
+        div.style.width = px(bitmap.getWidth());
+      }
+      if (div.style.height === "" && bitmap.getHeight()) {
+        div.style.height = px(bitmap.getHeight());
+      }
+    }
+    return div;
   }
 
   /*
