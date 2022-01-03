94aed7b2be4e6d7fe88090ac678616b9aa752e68
Jordan Eldredge <jordan@jordaneldredge.com>
2022-01-03 12:59:39 -0800

    Don't do debug logs in prod

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-console.log(params)
 
 export const SCREENSHOT_WIDTH = 275;
 export const SCREENSHOT_HEIGHT = 348;
