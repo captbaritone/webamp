3cad5d280e75afbd78a89abf641ac58ebdb1f4c2
Jordan Eldredge <jordan@jordaneldredge.com>
2022-03-09 23:36:16 -0800

    Just try to get GitHub Actions to tell us what we have here

--- packages/skin-database/db.ts
+++ packages/skin-database/db.ts
@@ -2,6 +2,6 @@ import Knex from "knex";
 import * as knexConfigs from "./knexfile";
 import { NODE_ENV } from "./config";
 
-console.log({ NODE_ENV });
+throw new Error(`NODE_ENV: ${NODE_ENV}`);
 
 export const knex = Knex(knexConfigs[NODE_ENV]);
