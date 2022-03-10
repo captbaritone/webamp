29ba129cb04aad350914c2fa54c8b7d110ce9f3e
Jordan Eldredge <jordan@jordaneldredge.com>
2022-03-09 21:57:32 -0800

    Log node env

--- packages/skin-database/db.ts
+++ packages/skin-database/db.ts
@@ -2,4 +2,6 @@ import Knex from "knex";
 import * as knexConfigs from "./knexfile";
 import { NODE_ENV } from "./config";
 
+console.log({ NODE_ENV });
+
 export const knex = Knex(knexConfigs[NODE_ENV]);
--- packages/skin-database/jest-setup.js
+++ packages/skin-database/jest-setup.js
@@ -25,4 +25,3 @@ process.env.TWITTER_ACCESS_TOKEN_SECRET = "";
 process.env.INSTAGRAM_ACCESS_TOKEN = "";
 process.env.INSTAGRAM_ACCOUNT_ID = "";
 process.env.SECRET = "";
-process.env.NODE_ENV = "test";
