d5a3b8646f3b3c781e28ea2d940985cd988f5b48
Jordan Eldredge <jordan@jordaneldredge.com>
2022-03-09 23:40:48 -0800

    Try falling back to test

--- packages/skin-database/db.ts
+++ packages/skin-database/db.ts
@@ -2,6 +2,4 @@ import Knex from "knex";
 import * as knexConfigs from "./knexfile";
 import { NODE_ENV } from "./config";
 
-throw new Error(`NODE_ENV: ${NODE_ENV}`);
-
-export const knex = Knex(knexConfigs[NODE_ENV]);
+export const knex = Knex(knexConfigs[NODE_ENV ?? "test"]);
