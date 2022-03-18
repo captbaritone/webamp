f8559f0b028406c466066b641a45b641abd186e7
Fathony <fathony@smart-leaders.net>
2022-03-18 13:18:47 +0700

    allow external *.wal sample: http://localhost:8080/?skin=/assets/extracted/skins/MMD3.wal

--- packages/webamp-modern-2/src/utils.ts
+++ packages/webamp-modern-2/src/utils.ts
@@ -80,6 +80,20 @@ export function findLast<T>(
   }
 }
 
+
+export function getUrlQuery(location:Location, variable:string):string {
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+          return decodeURIComponent(pair[1]);
+      }
+  }
+  console.log('Query variable %s not found', variable);
+  return null;
+}
+
 export class Emitter {
   _cbs: { [event: string]: Array<() => void> } = {};
   on(event: string, cb: () => void) {
