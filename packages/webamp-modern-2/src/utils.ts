e0724c338cd8664d4a2a86b68f13af9bb9598806
Fathony Luthfillah <x2nie@yahoo.com>
2022-03-18 13:58:29 +0700

    Update packages/webamp-modern-2/src/utils.ts
    okay, one line is better. less code less bug
    
    Co-authored-by: Jordan Eldredge <jordan@jordaneldredge.com>

--- packages/webamp-modern-2/src/utils.ts
+++ packages/webamp-modern-2/src/utils.ts
@@ -82,16 +82,7 @@ export function findLast<T>(
 
 
 export function getUrlQuery(location:Location, variable:string):string {
-  var query = location.search.substring(1);
-  var vars = query.split('&');
-  for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
-      if (decodeURIComponent(pair[0]) == variable) {
-          return decodeURIComponent(pair[1]);
-      }
-  }
-  console.log('Query variable %s not found', variable);
-  return null;
+  return new URL(location.href).searchParams.get(variable)
 }
 
 export class Emitter {
