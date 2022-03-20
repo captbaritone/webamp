ffdb574eacd25de0c322eb7d2924a058248b5708
Fathony <fathony@smart-leaders.net>
2022-03-20 11:20:23 +0700

    +comment, prettier

--- packages/webamp-modern-2/src/maki/interpreter.ts
+++ packages/webamp-modern-2/src/maki/interpreter.ts
@@ -311,14 +311,12 @@ class Interpreter {
             methodArgs.push(a.value);
           }
           const obj = this.stack.pop();
-          if(!!!obj.value/* ==null */ 
-            //&& (methodName==='newitem' || methodName==='newattribute'|| methodName==='getparentlayout')
-            && ( 
-                (klass.name || '').toLowerCase() == 'winampconfig' || 
-                (klass.name || '').toLowerCase() == 'winampconfiggroup' || 
-                (klass.name || '').toLowerCase() == 'configclass' 
-                )
-            ){
+          if (
+            !obj.value &&
+            ((klass.name || "").toLowerCase() == "winampconfig" ||
+              (klass.name || "").toLowerCase() == "winampconfiggroup" ||
+              (klass.name || "").toLowerCase() == "configclass")
+          ) {
             obj.value = new klass();
           }
           assert(
