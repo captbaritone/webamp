db7ba2830d00d6a8b5f660f70a201427d286149f
Fathony <fathony@smart-leaders.net>
2022-03-20 15:39:39 +0700

    +WinampConfig files

---
+++ packages/webamp-modern-2/src/skin/makiClasses/WinampConfig.ts
@@ -0,0 +1,36 @@
+import XmlObj from "../XmlObj";
+import ConfigItem from "./ConfigItem";
+
+const _items : {[key:string]: ConfigItem} = {};
+
+export default class WinampConfig {
+  static GUID = "b2ad3f2b4e3131ed95e96dbcbb55d51c";
+  // _items : {[key:string]: ConfigItem} = {};
+
+  getgroup(config_group_guid: string): WinampConfigGroup {
+   return null;
+  }
+
+  
+}
+
+export class WinampConfigGroup {
+    static GUID = 'fc17844e4518c72bf9a868a080baa530';
+
+    getbool(itemName: string): boolean {
+        return true;
+    }
+
+    getint(itemName: string): number {
+        return 0;
+    }
+
+    getstring(itemName: string): string {
+        return '';
+    }
+
+}
+
+// Global Singleton for now
+// export const Config = new ConfigClass();
+// export Config;
