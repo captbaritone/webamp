cae2922d6e8f6af0365cc4109d4c6841ee62d7ff
Fathony <fathony@smart-leaders.net>
2022-03-23 09:34:30 +0700

    colortheme: doubleclickable,bold,header-column

--- packages/webamp-modern-2/src/skin/ColorThemesList.ts
+++ packages/webamp-modern-2/src/skin/ColorThemesList.ts
@@ -1,23 +1,40 @@
 import GuiObj from "./makiClasses/GuiObj";
 import UI_ROOT from "../UIRoot";
-import { removeAllChildNodes } from "../utils";
+import { removeAllChildNodes, toBool } from "../utils";
 
 export default class ColorThemesList extends GuiObj {
   _select: HTMLSelectElement = document.createElement("select");
+  _nohscroll: boolean = false;
+  _nocolheader: boolean = false;
 
   constructor() {
     super();
     this._div.appendChild(this._select);
+    this._registerEvents();
   }
   setXmlAttr(key: string, value: string): boolean {
     if (super.setXmlAttr(key, value)) {
       return true;
     }
-    switch (key) {
+    const _key = key.toLowerCase();
+    switch (_key) {
+      // see skin: D-Reliction
+      case "nocolheader":
+        this._nocolheader = toBool(value);
+        break;
+      case "nohscroll":
+        this._nohscroll = toBool(value);
+        break;
       default:
         return false;
     }
     return true;
+  }
+
+  _registerEvents() {
+    this._select.addEventListener("dblclick", () => {
+      this.handleAction("colorthemes_switch");
+    });
   }
 
   _renderGammaSets() {
@@ -26,25 +43,54 @@ export default class ColorThemesList extends GuiObj {
     this._select.style.position = "absolute";
     this._select.style.width = "100%";
     this._select.style.height = "100%";
+
     for (const key of UI_ROOT._gammaSets.keys()) {
       const option = document.createElement("option");
       option.value = key;
       option.innerText = UI_ROOT._gammaNames[key];
       this._select.appendChild(option);
     }
+
+    // Overflow
+    this._select.style.overflowY = "scroll";
+    if (this._nohscroll) {
+      this._select.style.overflowX = "hidden";
+    } else {
+      this._select.style.overflowX = "scroll";
+    }
+
+    // column header
+    if (this._nocolheader) {
+      this._select.style.setProperty("--colheader", null);
+    } else {
+      this._select.style.setProperty("--colheader", "'Theme'");
+    }
+
     this._select.value = UI_ROOT._activeGammaSetName;
+    this._renderBoldSelection();
+  }
+
+  _renderBoldSelection() {
+    Array.from(this._select.options).forEach(function (option_element) {
+      if (option_element.value === UI_ROOT._activeGammaSetName) {
+        option_element.setAttribute("selected", "selected");
+      } else {
+        option_element.removeAttribute("selected");
+      }
+    });
   }
 
   handleAction(
     action: string,
-    param: string | null,
-    actionTarget: string | null
+    param: string | null = null,
+    actionTarget: string | null = null
   ) {
     switch (action) {
       case "colorthemes_switch":
         const selected = this._select.value;
         if (selected != null) {
           UI_ROOT.enableGammaSet(selected);
+          this._renderBoldSelection();
         }
         return true;
     }
