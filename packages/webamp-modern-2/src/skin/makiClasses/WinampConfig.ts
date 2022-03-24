ad4e3123f3ae6ca462561606226925e7af84d31a
Fathony <fathony@smart-leaders.net>
2022-03-24 13:19:18 +0700

    less error/warn. prettier.

--- packages/webamp-modern-2/src/skin/makiClasses/WinampConfig.ts
+++ packages/webamp-modern-2/src/skin/makiClasses/WinampConfig.ts
@@ -1,34 +1,30 @@
-import XmlObj from "../XmlObj";
 import ConfigItem from "./ConfigItem";
 
-const _items : {[key:string]: ConfigItem} = {};
+const _items: { [key: string]: ConfigItem } = {};
 
 export default class WinampConfig {
   static GUID = "b2ad3f2b4e3131ed95e96dbcbb55d51c";
   // _items : {[key:string]: ConfigItem} = {};
 
   getgroup(config_group_guid: string): WinampConfigGroup {
-   return null;
+    return new WinampConfigGroup();
   }
-
-  
 }
 
 export class WinampConfigGroup {
-    static GUID = 'fc17844e4518c72bf9a868a080baa530';
+  static GUID = "fc17844e4518c72bf9a868a080baa530";
 
-    getbool(itemName: string): boolean {
-        return true;
-    }
+  getbool(itemName: string): boolean {
+    return true;
+  }
 
-    getint(itemName: string): number {
-        return 0;
-    }
+  getint(itemName: string): number {
+    return 0;
+  }
 
-    getstring(itemName: string): string {
-        return '';
-    }
-
+  getstring(itemName: string): string {
+    return "";
+  }
 }
 
 // Global Singleton for now
