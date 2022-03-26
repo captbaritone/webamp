49a11c12ac49ca5659a3b56c1f633af1688fb6b9
Fathony <fathony@smart-leaders.net>
2022-03-26 11:43:01 +0700

    add api: getAttribute

--- packages/webamp-modern-2/src/skin/makiClasses/ConfigItem.ts
+++ packages/webamp-modern-2/src/skin/makiClasses/ConfigItem.ts
@@ -1,23 +1,23 @@
 import XmlObj from "../XmlObj";
 import ConfigAttribute from "./ConfigAttribute";
 
-
 export default class ConfigItem {
   static GUID = "d40302824d873aab32128d87d5fcad6f";
-  _name : string;
-  _items : {[key:string]: ConfigAttribute} = {};
-
-//   constructor(name:string) {
-  // constructor() {
-    // super();
-    // this._name = name;
-    // this._items = {};
-  // }
+  _name: string;
+  _items: { [key: string]: ConfigAttribute } = {};
 
   newattribute(name: string, defaultValue: string): ConfigAttribute {
     const cfg = new ConfigAttribute(name, defaultValue);
     // const cfg = new ConfigAttribute();
     // this._items[name] = cfg;
     return cfg;
+  }
+
+  getattribute(att_name: string): ConfigAttribute {
+    // sample:
+    // ConfigItem ciMisc;
+    // ciMisc = Config.getItem("Options");
+    // configAttribute_system_repeatType = ciMisc.getAttribute("repeat");
+    return new ConfigAttribute(att_name, "1");
   }
 }
