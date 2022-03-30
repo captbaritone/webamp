ccbf0f37a0e32ef9d89a46365a80d14de4c4b03c
Fathony <fathony@smart-leaders.net>
2022-03-30 13:39:28 +0700

    reconfigure objectData target path

--- packages/webamp-modern-2/tools/extract-object-types.js
+++ packages/webamp-modern-2/tools/extract-object-types.js
@@ -2,7 +2,7 @@ var parser = require("./parse-mi");
 var path = require("path");
 var fs = require("fs");
 
-const compilers = path.join(__dirname, "../../webamp-modern/resources/maki_compiler/");
+const compilers = path.join(__dirname, "../resources/maki_compiler/");
 
 const lib566 = path.join(compilers, "v1.2.0 (Winamp 5.66)/lib/");
 
@@ -17,7 +17,7 @@ const files = {
 Object.keys(files).forEach((name) => {
   const sourcePath = files[name];
   const types = parser.parseFile(sourcePath);
-  const destinationPath = path.join(__dirname, `../objectData/${name}.json`);
+  const destinationPath = path.join(__dirname, `../src/maki/objectData/${name}.json`);
 
   fs.writeFileSync(destinationPath, JSON.stringify(types, null, 2));
 });
