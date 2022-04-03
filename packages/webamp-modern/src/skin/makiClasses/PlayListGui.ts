4839ce40d6d40ca48aba16cbbba53a634a1c74c3
Fathony <fathony@smart-leaders.net>
2022-04-03 22:45:24 +0700

    plEdit: selected & currrent colors.

--- packages/webamp-modern/src/skin/makiClasses/PlayListGui.ts
+++ packages/webamp-modern/src/skin/makiClasses/PlayListGui.ts
@@ -8,6 +8,7 @@ import GuiObj from "./GuiObj";
 export default class PlayListGui extends GuiObj {
   static GUID = "pl";
   static guid = "{45F3F7C1-A6F3-4EE6-A15E-125E92FC3F8D}";
+  _selectedIndex: number = -1;
 
 
   getElTag(): string {
@@ -24,16 +25,37 @@ export default class PlayListGui extends GuiObj {
   refresh = () => {
     removeAllChildNodes(this._div);
     const pl = UI_ROOT.playlist;
+    const currentTrack = pl.getcurrentindex()
     for(let i = 0; i < pl.getnumtracks(); i++){
       const line = document.createElement('div');
+      if(i==currentTrack){
+        line.classList.add('current')
+      }
+      if(i==this._selectedIndex){
+        line.classList.add('selected')
+      }
+      line.addEventListener('click', (ev:MouseEvent)=>{
+        this._selectedIndex = i;
+        this.refresh()
+      })
+      line.addEventListener('dblclick', (ev:MouseEvent)=>{
+        // this._selectedIndex = i;
+        UI_ROOT.playlist.playtrack(i)
+        this.refresh()
+      })
       line.textContent = pl.gettitle(i);
       this._div.appendChild(line)
     }
   }
 
+  itemClick = () => {
+
+  }
+  
   draw() {
     super.draw()
-
+    
+    this._div.setAttribute('tabindex', '0')
     this._div.classList.add('pl')
     this._div.classList.add('list')
   }
