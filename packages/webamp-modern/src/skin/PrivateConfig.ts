6e047606adc34822dfc344aa31a10582157ad6c9
Fathony <fathony@smart-leaders.net>
2022-04-14 22:24:23 +0700

    Complete imp PrivateConfig: Persistent Storage

--- packages/webamp-modern/src/skin/PrivateConfig.ts
+++ packages/webamp-modern/src/skin/PrivateConfig.ts
@@ -1,26 +1,73 @@
-// TODO: Persist this to local state?
 class PrivateConfig {
-  _sections: Map<string, Map<string, string>> = new Map();
-  _getSection(section: string) {
-    if (!this._sections.has(section)) {
-      this._sections.set(section, new Map());
+  // _sections: Map<string, Map<string, string|number>> = new Map();
+  _sections: { [section: string]: { [key: string]: string | number } };
+
+  constructor() {
+    // Retrieve the object from storage
+    const cookies = localStorage.getItem("_privateConfig_");
+    if (cookies) {
+      this._sections = JSON.parse(cookies);
+    } else {
+      this._sections = {};
     }
-    return this._sections.get(section);
-  }
-  getPrivateInt(section: string, item: string, defvalue: number):number {
-    const value:string = this._getSection(section).get(item);
-    return value? parseInt(value) : defvalue ;
   }
 
-  setPrivateInt(section: string, item: string, value: number) {
-    return this._getSection(section).set(item, value.toString());
+  _saveState = debounce(() => {
+    // Put the object into storage
+    localStorage.setItem("_privateConfig_", JSON.stringify(this._sections));
+  }, 100);
+
+  _getSection(section: string) {
+    if (this._sections[section] == null) {
+      this._sections[section] = {};
+    }
+    return this._sections[section];
+  }
+
+  getPrivateInt(section: string, item: string, defvalue: number): number {
+    const value: string | number = this._getSection(section)[item];
+    // if not found, create new one
+    if (value == null) {
+      return this.setPrivateInt(section, item, defvalue);
+    }
+    return Number(value);
+  }
+
+  setPrivateInt(section: string, item: string, value: number): number {
+    value = Number(value); // guard
+    this._getSection(section)[item] = value;
+    this._saveState();
+    return value;
   }
 
   getPrivateString(section: string, item: string, defvalue: string): string {
-    return this._getSection(section).get(item) ?? defvalue;
+    const value: string | number = this._getSection(section)[item];
+    // if not found, create new one
+    if (value == null) {
+      return this.setPrivateString(section, item, defvalue);
+    }
+    return String(value);
   }
 
+  setPrivateString(section: string, item: string, value: string): string {
+    value = String(value); // guard
+    this._getSection(section)[item] = value;
+    this._saveState();
+    return value;
+  }
+}
 
+function debounce<Params extends any[]>(
+  func: (...args: Params) => any,
+  timeout: number
+): (...args: Params) => void {
+  let timer: NodeJS.Timeout;
+  return (...args: Params) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
 }
 
 const PRIVATE_CONFIG = new PrivateConfig();
