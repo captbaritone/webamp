20a95763b91aca1da2f370b7a971c34bb4a78b5f
Fathony <fathony@smart-leaders.net>
2022-04-18 22:40:53 +0700

    del dead code

--- packages/webamp-modern/src/skin/makiClasses/MapObj.ts
+++ packages/webamp-modern/src/skin/makiClasses/MapObj.ts
@@ -1,20 +1,8 @@
-import UI_ROOT from "../../UIRoot";
-import { assert, px, removeAllChildNodes, toBool } from "../../utils";
-import Layout from "./Layout";
 import XmlObj from "../XmlObj";
 
-// > A container is a top level object and it basically represents a window.
-// > Nothing holds a container. It is an object that holds multiple related
-// > layouts. Each layout represents an appearance for that window. You can design
-// > different layouts for each window but only one can be visible at a time.
-//
-// -- http://wiki.winamp.com/wiki/Modern_Skin
 export default class MapObj extends XmlObj {
   static GUID = "38603665461B42a7AA75D83F6667BF73";
-  
-  _layouts: Layout[] = [];
-  _activeLayout: Layout | null = null;
-  _visible: boolean = true;
+
   _id: string;
   _name: string;
   constructor() {
@@ -33,13 +21,9 @@ export default class MapObj extends XmlObj {
       case "id":
         this._id = value.toLowerCase();
         break;
-      case "default_visible":
-        this._visible = toBool(value);
-        break;
       default:
         return false;
     }
     return true;
   }
-
 }
