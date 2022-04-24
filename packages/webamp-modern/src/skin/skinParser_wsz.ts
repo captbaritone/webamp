bad60f24b22fc432fc44094c314d1e7600fc3ad3
Fathony <fathony@smart-leaders.net>
2022-04-25 00:56:53 +0700

    equalizer

--- packages/webamp-modern/src/skin/skinParser_wsz.ts
+++ packages/webamp-modern/src/skin/skinParser_wsz.ts
@@ -11,18 +11,20 @@ export default class ClassicSkinParser extends SkinParser {
   _wszRoot: string = "/assets/winamp_classic/";
 
   constructor(uiRoot: UIRoot) {
-      super(uiRoot)
-      // load internal wsz prototype from:
-      uiRoot.setSkinDir("assets/winamp_classic/")
+    super(uiRoot);
+    // load internal wsz prototype from:
+    uiRoot.setSkinDir("assets/winamp_classic/");
   }
 
   async _internalFile(fileName: string): Promise<string> {
-    return await this._uiRoot.getFileAsStringPath(/* this._wszRoot + */ fileName);
+    return await this._uiRoot.getFileAsStringPath(
+      /* this._wszRoot + */ fileName
+    );
   }
 
   async _parseInternalFile(fileName: string) {
     let content = await this._internalFile(fileName);
-    content = content.replace(/wsz_root\//ig,'')
+    content = content.replace(/wsz_root\//gi, "");
     // Note: Included files don't have a single root node, so we add a synthetic one.
     // A different XML parser library might make this unnessesary.
     const parsed = parseXmlFragment(content);
@@ -35,7 +37,12 @@ export default class ClassicSkinParser extends SkinParser {
 
     console.log("RESOURCE_PHASE #################");
     this._phase = RESOURCE_PHASE;
+
+    // Load built-in xui elements
+    await this.loadFreeformXui();
+
     await this._parseInternalFile("xml/player-elements.xml");
+    await this._parseInternalFile("xml/eq-elements.xml");
 
     await this._solveMissingBitmaps();
     await this._imageManager.loadUniquePaths();
@@ -45,7 +52,7 @@ export default class ClassicSkinParser extends SkinParser {
     this._phase = GROUP_PHASE;
     await this._parseInternalFile("xml/player.xml");
     //eqmain.bmp seem as not yet loaded, temporary disable:
-    // await this._parseInternalFile("eq.xml");
+    await this._parseInternalFile("xml/eq.xml");
 
     console.log("BUCKET_PHASE #################");
     await this.rebuildBuckets();
@@ -66,21 +73,20 @@ export default class ClassicSkinParser extends SkinParser {
 
   //special case, wsz never use external/linked bitmap in its filename
   _isExternalBitmapFont(font: BitmapFont) {
-    return false
+    return false;
   }
 
-//   async bitmapFont(node: XmlElement) {
-//     // assume(
-//     //   node.children.length === 0,
-//     //   "Unexpected children in <bitmapFont> XML node."
-//     // );
-//     const font = new BitmapFont();
-//     font.setXmlAttributes(node.attributes);
+  //   async bitmapFont(node: XmlElement) {
+  //     // assume(
+  //     //   node.children.length === 0,
+  //     //   "Unexpected children in <bitmapFont> XML node."
+  //     // );
+  //     const font = new BitmapFont();
+  //     font.setXmlAttributes(node.attributes);
 
-//       this._imageManager.addBitmap(font);
-//     }
+  //       this._imageManager.addBitmap(font);
+  //     }
 
-//     this._uiRoot.addFont(font);
-//   }
-
+  //     this._uiRoot.addFont(font);
+  //   }
 }
