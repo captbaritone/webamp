9014910964b1420208377534ac7bf5b7acb4759e
Fathony <fathony@smart-leaders.net>
2022-04-30 21:23:01 +0700

    first time real VIS (not random)

--- packages/webamp-modern/src/skin/makiClasses/Vis.ts
+++ packages/webamp-modern/src/skin/makiClasses/Vis.ts
@@ -263,11 +263,51 @@ class NoVisualizer extends VisPainter {
 
 //? =============================== BAR PAINTER ===============================
 const NUM_BARS = 20;
+const PIXEL_DENSITY = 1;
+const BAR_PEAK_DROP_RATE = 0.01;
+
+function octaveBucketsForBufferLength(bufferLength: number): number[] {
+  const octaveBuckets = new Array(NUM_BARS).fill(0);
+  const minHz = 200;
+  const maxHz = 22050;
+  const octaveStep = Math.pow(maxHz / minHz, 1 / NUM_BARS);
+
+  octaveBuckets[0] = 0;
+  octaveBuckets[1] = minHz;
+  for (let i = 2; i < NUM_BARS - 1; i++) {
+    octaveBuckets[i] = octaveBuckets[i - 1] * octaveStep;
+  }
+  octaveBuckets[NUM_BARS - 1] = maxHz;
+
+  for (let i = 0; i < NUM_BARS; i++) {
+    const octaveIdx = Math.floor((octaveBuckets[i] / maxHz) * bufferLength);
+    octaveBuckets[i] = octaveIdx;
+  }
+
+  return octaveBuckets;
+}
+
 class BarPainter extends VisPainter {
+  _analyser: AnalyserNode;
   _barWidth: number;
   _color: string = "rgb(255,255,255)";
   // Off-screen canvas for pre-rendering a single bar gradient
   _bar: HTMLCanvasElement = document.createElement("canvas");
+  _peak: HTMLCanvasElement = document.createElement("canvas");
+  _barPeaks: number[] = new Array(NUM_BARS).fill(0);
+  _barPeakFrames: number[] = new Array(NUM_BARS).fill(0);
+  _bufferLength: number;
+  _octaveBuckets: number[];
+  _dataArray: Uint8Array;
+
+  constructor(vis: Vis) {
+    super(vis);
+    // this.prepare();
+    this._analyser = UI_ROOT.audio.getAnalyser();
+    this._bufferLength = this._analyser.frequencyBinCount;
+    this._octaveBuckets = octaveBucketsForBufferLength(this._bufferLength);
+    this._dataArray = new Uint8Array(this._bufferLength);
+  }
 
   prepare() {
     const vis = this._vis;
@@ -275,6 +315,15 @@ class BarPainter extends VisPainter {
     const gammaGroup = UI_ROOT._getGammaGroup(groupId);
     this._barWidth = Math.ceil(vis._canvas.width / NUM_BARS);
     // this._barWidth = vis._canvas.width / NUM_BARS;
+
+    //? paint peak
+    this._peak.height = 1;
+    this._peak.width = 1;
+    var ctx = this._peak.getContext("2d");
+    ctx.fillStyle = this._color;
+    ctx.fillRect(0, 0, 1, 1);
+
+    //? paint bar
     this._bar.height = vis._canvas.height;
     this._bar.width = 1;
     var ctx = this._bar.getContext("2d");
@@ -286,6 +335,7 @@ class BarPainter extends VisPainter {
         gammaGroup.transformColor(vis._colorBands[i])
       );
     }
+
     // if (vis._colorBands[0]) {
     //   this._color = `rgba(${vis._colorBands[0]}, 1)`;
 
@@ -300,6 +350,7 @@ class BarPainter extends VisPainter {
     //   }
     // }
     // ctx.fillStyle = this._color;
+    ctx.strokeStyle = this._color;
     ctx.fillStyle = grd;
     ctx.fillRect(0, 0, 1, vis._canvas.height);
     //debug:
@@ -314,13 +365,79 @@ class BarPainter extends VisPainter {
     // ctx.fillStyle = 'white';
     // ctx.fillRect(0,0,w,h);
     ctx.fillStyle = this._color;
+
+    this._analyser.getByteFrequencyData(this._dataArray);
+    const heightMultiplier = h / 256;
+    const xOffset = this._barWidth + PIXEL_DENSITY; // Bar width, plus a pixel of spacing to the right.
+    for (let j = 0; j < NUM_BARS - 1; j++) {
+      const start = this._octaveBuckets[j];
+      const end = this._octaveBuckets[j + 1];
+      let amplitude = 0;
+      for (let k = start; k < end; k++) {
+        amplitude += this._dataArray[k];
+      }
+      amplitude /= end - start;
+
+      // The drop rate should probably be normalized to the rendering FPS, for now assume 60 FPS
+      let barPeak =
+        this._barPeaks[j] -
+        BAR_PEAK_DROP_RATE * Math.pow(this._barPeakFrames[j], 2);
+      if (barPeak < amplitude) {
+        barPeak = amplitude;
+        this._barPeakFrames[j] = 0;
+      } else {
+        this._barPeakFrames[j] += 1;
+      }
+      this._barPeaks[j] = barPeak;
+
+      this.paintBar(
+        ctx,
+        j /* * xOffset */,
+        amplitude * heightMultiplier,
+        barPeak * heightMultiplier
+      );
+    }
+  }
+
+  paintBar(
+    ctx: CanvasRenderingContext2D,
+    barIndex: number,
+    barHeight: number,
+    peakHeight: number
+  ) {
+    const w = ctx.canvas.width;
+    const h = ctx.canvas.height;
+    var x = Math.round(this._barWidth * barIndex);
+    var r = this._barWidth - 2;
+    var x2 = Math.round(this._barWidth * (barIndex + 1)) - 2;
+    // var y = Math.ceil(Math.random() * h);
+    var y = h - barHeight;
+
+    ctx.drawImage(this._bar, x, y, x2 - x + 1, h - y);
+
+    const peakY = h - peakHeight;
+    ctx.drawImage(this._peak, 0,0,1,1, x, peakY, x2 - x + 1, 1);
+
+    // ctx.beginPath();
+    // ctx.moveTo(x, peakHeight);
+    // ctx.lineTo(x2, peakHeight);
+    // ctx.stroke();
+  }
+
+  paintFrame1(ctx: CanvasRenderingContext2D) {
+    const w = ctx.canvas.width;
+    const h = ctx.canvas.height;
+    // this._barWidth = Math.ceil(w / NUM_BARS);
+    ctx.clearRect(0, 0, w, h);
+    // ctx.fillStyle = 'white';
+    // ctx.fillRect(0,0,w,h);
+    ctx.fillStyle = this._color;
     for (var i = 0; i < NUM_BARS; i++) {
       var x = Math.round(this._barWidth * i);
       var r = this._barWidth - 2;
       var x2 = Math.round(this._barWidth * (i + 1)) - 2;
-      // var x = Math.floor(Math.random() * ctx.canvas.width - 17);
-      // var y = Math.floor(Math.random() * h);
       var y = Math.ceil(Math.random() * h);
+
       // var y = Math.ceil(i / NUM_BARS * h);
       // var radius = Math.floor(Math.random() * 20);
 
