33bfafceaf2a1549d7104ceab60c0f384c11246b
Fathony <fathony@smart-leaders.net>
2022-05-09 22:55:43 +0700

    bugfix repo inside temp

--- packages/webamp-modern/jest.config.js
+++ packages/webamp-modern/jest.config.js
@@ -1,13 +1,14 @@
 module.exports = {
     displayName: "webamp-modern-test",
     // rootDir: path.join(__dirname, "/packages/webamp-modern/src"),
+    rootDir: "./",
     testRegex: "\\.test\\.(js|ts)$",
     moduleFileExtensions: ["js", "ts"],
     testPathIgnorePatterns: [
       "/node_modules/",
       "build",
       "dist",
-      "temp",
+      "/temp/",
     ],
     // testEnvironment: "jsdom",
     // setupFiles: ["<rootDir>/packages/skin-database/jest-setup.js"],
