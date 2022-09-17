captbaritone/webamp

commit bedc983d2ad9084592743e9f77a7eab995fbd0f4

Small cleanup of WinampButton
Missing hook dependency, and use default destructuring instead of defaultProps

packages/webamp/js/components/WinampButton.tsx
@@ -31,13 +31,12 @@ type Props = DetailedHTMLPropsAndMore;
  * > and the time it is depressed; for a finger in a multitouch environment, while
  * > the finger is touching the display surface).
  */
-export default function WinampButton(props: Props): JSX.Element {
+export default function WinampButton({
+  requireClicksOriginateLocally = true,
+  onPointerDown: originalOnPointerDown,
+  ...htmlProps
+}: Props): JSX.Element {
   const [active, setActive] = useState(false);
-  const {
-    requireClicksOriginateLocally,
-    onPointerDown: originalOnPointerDown,
-    ...htmlProps
-  } = props;
 
   const onPointerDown = useCallback(
     (e) => {
@@ -66,7 +65,7 @@ export default function WinampButton(props: Props): JSX.Element {
       }
       document.addEventListener("pointerup", onRelease);
     },
-    [originalOnPointerDown]
+    [originalOnPointerDown, requireClicksOriginateLocally]
   );
 
   // We watch for events onPointerEnter only when requireClicksOriginateLocally === false
@@ -97,7 +96,3 @@ export default function WinampButton(props: Props): JSX.Element {
     />
   );
 }
-
-WinampButton.defaultProps = {
-  requireClicksOriginateLocally: true,
-};

commit 0dc443666adde941f8a68ee3b32282689df66e02

Fix orphaned className
I broke this in https://github.com/captbaritone/webamp/commit/bedc983d2ad9084592743e9f77a7eab995fbd0f4

packages/webamp/js/components/WinampButton.tsx
@@ -34,6 +34,7 @@ type Props = DetailedHTMLPropsAndMore;
 export default function WinampButton({
   requireClicksOriginateLocally = true,
   onPointerDown: originalOnPointerDown,
+  className,
   ...htmlProps
 }: Props): JSX.Element {
   const [active, setActive] = useState(false);
@@ -84,11 +85,10 @@ export default function WinampButton({
     }
   };
 
-  const className = classnames(props.className, { [ACTIVE_CLASSNAME]: active });
   return (
     <div
       {...htmlProps}
-      className={className}
+      className={classnames(className, { [ACTIVE_CLASSNAME]: active })}
       onPointerDown={onPointerDown}
       onPointerEnter={
         requireClicksOriginateLocally ? undefined : onPointerEnter