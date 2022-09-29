70af734d3aa3e8dd5245b37831d3d515a8b48bce
Jordan Eldredge <jordan@jordaneldredge.com>
2022-09-28 19:24:17 -0700

    Update deploy script to reflect new demo site dir

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ yarn workspace ani-cursor build
 yarn workspace webamp build
 yarn workspace webamp build-library
 yarn workspace webamp-modern build
-mv packages/webamp-modern/build packages/webamp/demo/built/modern
+mv packages/webamp-modern/build packages/webamp/dist/demo-site/modern
