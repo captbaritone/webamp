101997b787282e60062fc6c7f662dbdcd97a761f
Jordan Eldredge <jordan@jordaneldredge.com>
2023-01-20 10:03:11 -0800

    Fix bug where eject button active state was not shown

--- packages/webamp/js/components/MainWindow/Eject.tsx
+++ packages/webamp/js/components/MainWindow/Eject.tsx
@@ -2,10 +2,17 @@ import { memo } from "react";
 
 import * as Actions from "../../actionCreators";
 import { useActionCreator } from "../../hooks";
+import WinampButton from "../WinampButton";
 
 const Eject = memo(() => {
   const openMediaFileDialog = useActionCreator(Actions.openMediaFileDialog);
-  return <div id="eject" onClick={openMediaFileDialog} title="Open File(s)" />;
+  return (
+    <WinampButton
+      id="eject"
+      onClick={openMediaFileDialog}
+      title="Open File(s)"
+    />
+  );
 });
 
 export default Eject;
