2f032ff3855f58eafbf0a49cc6fa7183e7584128
Jordan Eldredge <jordan@jordaneldredge.com>
2023-03-04 11:03:07 -0800

    Add option in API to normalize file extension

--- packages/skin-database/api/graphql/resolvers/ModernSkinResolver.ts
+++ packages/skin-database/api/graphql/resolvers/ModernSkinResolver.ts
@@ -1,6 +1,7 @@
 import SkinModel from "../../../data/SkinModel";
 import CommonSkinResolver from "./CommonSkinResolver";
 import { NodeResolver, toId } from "./NodeResolver";
+import path from "path";
 
 export default class ModernSkinResolver
   extends CommonSkinResolver
@@ -10,5 +11,12 @@ export default class ModernSkinResolver
   __typename = "ModernSkin";
   async id() {
     return toId(this.__typename, this.md5());
+  }
+  async filename(normalize_extension = false) {
+    const filename = await this._model.getFileName();
+    if (normalize_extension) {
+      return path.parse(filename).name + ".wal";
+    }
+    return filename;
   }
 }
