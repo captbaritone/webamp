eb747b30390cfc4ac7f83148a299db100d97daca
Fathony Luthfillah <fathony@smart-leaders.net>
2023-03-09 18:24:39 +0700

    complete: auto compile (listen *.m changes)

--- packages/webamp-modern/snowpack-maki-plugin.js
+++ packages/webamp-modern/snowpack-maki-plugin.js
@@ -6,16 +6,62 @@ const execa = require('execa');
 var isWin = process.platform === "win32";
 console.log('IS WINDOWS:', isWin)
 
+const MC = 'resources/maki_compiler/v1.2.0 (Winamp 5.66)/mc.exe'//.split('/')
+
 // const IMPORT_REGEX = /\@(use|import|forward)\s*['"](.*?)['"]/g;
-// const PARTIAL_REGEX = /([\/\\])_(.+)(?![\/\\])/;
+const COMPILED = /\s+was\s+compiled\s+to\s+/;
 
 const compile = async (filePath) =>{
+    console.log('compiling ', filePath);
     const execaOptions = {}
-    const { stdout, stderr } = await execa('echo', ['unicorn'], execaOptions);
+    const start = isWin? MC : 'wine';
+    const args = isWin? [] : [MC];
+    args.push(filePath)
+    // const { stdout, stderr } = await execa('echo', ['unicorn'], execaOptions);
+    const { stdout, stderr } = await execa(start, args, execaOptions);
     // Handle the output.
     if (stderr) throw new Error(stderr);
     // if (stdout) return stdout;
-    console.log(stdout);    
+    if (stdout && COMPILED.test(stdout)) {
+        const src = getFileName(filePath+'aki')
+        const dst = filePath+'aki';
+        const callback = (err) => {
+            if (err) throw err
+            console.log('Successfully compiled:',src)
+          }
+        fs.rename(src, dst, callback)
+    }
+}
+function getFileName(str) {
+    return str.split('\\').pop().split('/').pop();
+}
+function move(oldPath, newPath, callback) {
+
+    fs.rename(oldPath, newPath, function (err) {
+        if (err) {
+            if (err.code === 'EXDEV') {
+                copy();
+            } else {
+                callback(err);
+            }
+            return;
+        }
+        callback();
+    });
+
+    function copy() {
+        var readStream = fs.createReadStream(oldPath);
+        var writeStream = fs.createWriteStream(newPath);
+
+        readStream.on('error', callback);
+        writeStream.on('error', callback);
+
+        readStream.on('close', function () {
+            fs.unlink(oldPath, callback);
+        });
+
+        readStream.pipe(writeStream);
+    }
 }
 
 module.exports = function sassPlugin(snowpackConfig, { native, compilerOptions = {} } = {}) {
@@ -29,10 +75,10 @@ module.exports = function sassPlugin(snowpackConfig, { native, compilerOptions =
         //     output: ['.maki'],
         // },
         onChange({ filePath }) {
-            console.log('snowpack-maki plugin changes: onChange:', filePath);
+            // console.log('snowpack-maki plugin changes: onChange:', filePath);
             if (filePath.endsWith('.m')) {
                 // do something when a JavaScript file changes
-                console.log('CHANGED COY:', filePath)
+                // console.log('CHANGED COY:', filePath)
                 compile(filePath)
             }
             return;
