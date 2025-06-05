cdcf240a967fc49e211035b52214c99ecf78bb59
Jordan Eldredge <jordan@jordaneldredge.com>
2025-06-05 19:54:10 -0400

    Add ecoystem file

---
+++ packages/skin-database/ecosystem.config.js
@@ -0,0 +1,28 @@
+// To toggle between blue and green deployments:
+// sudo vim /etc/apache2/sites-enabled/api.webamp.org-le-ssl.conf
+// Update port number
+// sudo systemctl reload apache2
+module.exports = {
+  apps: [
+    {
+      name: "skin-database-blue",
+      script: "yarn",
+      interpreter: "bash",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001,
+      },
+    },
+    {
+      name: "skin-database-green",
+      script: "yarn",
+      interpreter: "bash",
+      args: "start:next",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3002,
+      },
+    },
+  ],
+};
