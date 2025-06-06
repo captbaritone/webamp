ac497964b036ed30fdd40a74a08604a483a58619
Jordan Eldredge <jordan@jordaneldredge.com>
2025-06-06 10:04:44 -0700

    Implement algolia logo

--- packages/skin-database/app/SearchLogo.tsx
+++ packages/skin-database/app/SearchLogo.tsx
@@ -1,19 +1,32 @@
 "use client";
-// import { useWindowSize } from "../../skin-museum-client/src/hooks";
-// import AlgoliaLogo from "../../skin-museum-client/src/searchByAlgoliaDarkbBackground.svg";
-// import algoliaLogoSmallUrl from "../../skin-museum-client/src/searchByAlgoliaSmall.png";
+import { useWindowSize } from "../../skin-museum-client/src/hooks";
+import AlgoliaLogo from "../../skin-museum-client/src/searchByAlgoliaDarkbBackground.svg";
+import algoliaLogoSmallUrl from "../../skin-museum-client/src/searchByAlgoliaSmall.png";
 
 export default function SearchLogo() {
-  return null;
-  // const { windowWidth } = useWindowSize();
-  // if (windowWidth > 500) {
-  //   return <AlgoliaLogo.ReactComponent />;
-  // }
-  // return (
-  //   <img
-  //     alt="Search by Algolia"
-  //     style={{ width: 25, height: 25, paddingTop: 4 }}
-  //     src={algoliaLogoSmallUrl}
-  //   />
-  // );
+  const { windowWidth } = useWindowSize();
+  if (windowWidth > 500) {
+    return (
+      <img
+        alt="Search by Algolia"
+        style={{
+          width: AlgoliaLogo.width,
+          height: AlgoliaLogo.height,
+          paddingTop: 4,
+        }}
+        src={AlgoliaLogo.src}
+      />
+    );
+  }
+  return (
+    <img
+      alt="Search by Algolia"
+      style={{
+        width: 25,
+        height: 25,
+        paddingTop: 4,
+      }}
+      src={algoliaLogoSmallUrl.src}
+    />
+  );
 }
