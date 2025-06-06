7dd7f139a94dfc4563226bdc30a7d95f909ca74d
Jordan Eldredge <jordan@jordaneldredge.com>
2025-06-05 21:48:12 -0700

    Add metadata

--- packages/skin-database/app/[[...folderName]]/page.tsx
+++ packages/skin-database/app/[[...folderName]]/page.tsx
@@ -1,4 +1,39 @@
 import App from "../../../skin-museum-client/src/App";
+import type { Metadata } from "next";
+
+const DESCRIPTION =
+  "Infinite scroll through 80k Winamp skins with interactive preview";
+
+export function generateMetadata(): Metadata {
+  const images = [
+    {
+      alt: "Screenshot of many Winamp skins in a grid.",
+      url: "https://skin-museum-og-captbaritone-webamp.vercel.app/api/og",
+      width: "1844",
+      height: "1297",
+    },
+  ];
+  return {
+    title: "Winamp Skin Museum",
+    description: DESCRIPTION,
+    openGraph: {
+      title: "Skin Museum",
+      description: DESCRIPTION,
+      images,
+      type: "website",
+      siteName: "Winamp Skin Museum",
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@winampskins",
+      title: "Skin Museum",
+      description: DESCRIPTION,
+      creator: "@captbaritone",
+      images,
+    },
+  };
+}
+
 export default function Page() {
   return <App next={true} />;
 }
--- packages/skin-database/app/layout.tsx
+++ packages/skin-database/app/layout.tsx
@@ -1,6 +1,14 @@
 import ReduxContextProvider from "./redux";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  themeColor: "#000000",
+  width: "device-width",
+  initialScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: {
