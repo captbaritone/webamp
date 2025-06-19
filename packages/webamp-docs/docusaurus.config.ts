f0f8e88dd077c423d2ac4760549d6464a98539b5
Jordan Eldredge <jordan@jordaneldredge.com>
2025-06-19 00:17:59 -0700

    Set new docs domain

--- packages/webamp-docs/docusaurus.config.ts
+++ packages/webamp-docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://webamp.",
+  url: "https://docs.webamp.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
