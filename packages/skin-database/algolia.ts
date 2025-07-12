f75d52c0c65d75cfde740188f06d4084a9cb8ddf
Jordan Eldredge <jordan@jordaneldredge.com>
2025-07-12 11:52:26 -0700

    Throw early

--- packages/skin-database/algolia.ts
+++ packages/skin-database/algolia.ts
@@ -1,6 +1,8 @@
 import { algoliasearch } from "algoliasearch";
 import { ALGOLIA_ACCOUNT, ALGOLIA_KEY } from "./config";
 
-console.log("Algolia account:", JSON.stringify(ALGOLIA_ACCOUNT));
+if (!ALGOLIA_ACCOUNT || !ALGOLIA_KEY) {
+  throw new Error("Algolia account and key must be defined in config.js");
+}
 
 export const client = algoliasearch(ALGOLIA_ACCOUNT, ALGOLIA_KEY);
