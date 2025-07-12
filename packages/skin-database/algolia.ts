4260c28da8434a43b4f1bc4832f9f875bd54fb0e
Jordan Eldredge <jordan@jordaneldredge.com>
2025-07-12 11:42:22 -0700

    Can we see the account being used?

--- packages/skin-database/algolia.ts
+++ packages/skin-database/algolia.ts
@@ -1,4 +1,6 @@
 import { algoliasearch } from "algoliasearch";
 import { ALGOLIA_ACCOUNT, ALGOLIA_KEY } from "./config";
 
+console.log("Algolia account:", JSON.stringify(ALGOLIA_ACCOUNT));
+
 export const client = algoliasearch(ALGOLIA_ACCOUNT, ALGOLIA_KEY);
