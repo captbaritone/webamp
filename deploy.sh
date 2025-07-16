eaba9667e27cb42c324f07cc6aa9ddf8812be967
Jordan Eldredge <jordan@jordaneldredge.com>
2025-07-15 18:57:35 -0700

    Try to fix netlify deploy

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pnpm --filter ani-cursor build
-pnpm --filter webamp build
-pnpm --filter webamp build-library
-pnpm --filter webamp-modern build
-mv packages/webamp-modern/build packages/webamp/dist/demo-site/modern
